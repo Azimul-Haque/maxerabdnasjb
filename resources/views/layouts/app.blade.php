3bd2f46167d7735747da5eadd493b3664d56a5fb
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-06 02:18:44 +0600

    Commit Mon 06/06/2022  2:18:44.23 26304

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
             <a class="nav-link" href="{{ route('index.index') }}"  target="_blank">
-              <i class="fas fa-globe"></i>
+              <i class="fas fa-globe-outlined"></i>
             </a>
           </li>
           <li class="nav-item dropdown">
