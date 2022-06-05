6b7b38510b8f6fda27eeb76d334e0cc61a1730ce
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-06 01:58:42 +0600

    Commit Mon 06/06/2022  1:58:42.27 882

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -32,9 +32,7 @@ class DashboardController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except('clear');
-        $this->middleware(['admin'])->only('getUsers', 'storeUser', 'updateUser', 'deleteUser', 'deleteBalance', 'getCreditors', 'getSingleCreditor', 'getAddDuePage', 'deleteCreditorDue', 'getSiteCategorywise');
-
+        
     }
 
     /**
