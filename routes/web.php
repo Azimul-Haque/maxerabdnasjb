834fb5dfe2a1fc36190a719335ca5ea1f4f3f384
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-06 02:00:12 +0600

    Commit Mon 06/06/2022  2:00:12.27 21815

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return redirect()->route('index.index'); });
+Route::get('/', function () { return redirect()->route('index'); });
 
 Auth::routes();
 
