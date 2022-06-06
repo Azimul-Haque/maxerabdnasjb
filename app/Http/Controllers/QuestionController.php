b41d6fe04a8b303c616114a89b6e3acad84c726c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 02:14:37 +0600

    Commit Tue 06/07/2022  2:14:37.22 13783

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth')->except('clear');
+        $this->middleware(['admin'])->only('getUsers', 'storeUser', 'updateUser', 'deleteUser', 'deleteBalance', 'getCreditors', 'getSingleCreditor', 'getAddDuePage', 'deleteCreditorDue', 'getSiteCategorywise');
+
+    }
 }
