51ded43045090f4400809f1062563eec6b0d647c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 02:15:39 +0600

    Commit Tue 06/07/2022  2:15:39.29 17021

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -9,7 +9,7 @@ class QuestionController extends Controller
     public function __construct()
     {
         $this->middleware('auth')->except('clear');
-        $this->middleware(['admin'])->only('getUsers', 'storeUser', 'updateUser', 'deleteUser', 'deleteBalance', 'getCreditors', 'getSingleCreditor', 'getAddDuePage', 'deleteCreditorDue', 'getSiteCategorywise');
+        $this->middleware(['admin'])->only('getUsers');
 
     }
 }
