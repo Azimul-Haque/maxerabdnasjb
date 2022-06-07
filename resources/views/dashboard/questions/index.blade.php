0b32edb5725f964417cef5fdee50fb719eb8b9eb
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-08 02:09:23 +0600

    Commit Wed 06/08/2022  2:09:23.26 21616

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -133,7 +133,7 @@
                                                           </div>
                                                           <div class="col-md-6">
                                                               <label for="explanation">ব্যাখ্যা (প্রয়োজনে)</label><br/>
-                                                              <textarea class="form-control" name="explanation" id="explanation" placeholder="ব্যাখ্যা" style="width: 100%; height: 220px;"></textarea>
+                                                              <textarea class="form-control" name="explanation" id="explanation" placeholder="ব্যাখ্যা" style="width: 100%; height: 220px;">{{ $question->questionexplanation ? $question->questionexplanation->explanation : '' }}</textarea>
                                                           </div>
                                                       </div>
                                                 </div>
