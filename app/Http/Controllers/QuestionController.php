0ac524268152495a30bfcaefbe5cdae9e5f9e6e9
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 17:05:58 +0600

    Commit Tue 06/07/2022 17:05:58.23 4778

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -35,7 +35,7 @@ class QuestionController extends Controller
         $topics = Topic::all();
 
         return view('dashboard.questions.index')
-                    ->withUsers($questions)
-                    ->withBalances($topics);
+                    ->withQuestions($questions)
+                    ->withTopics($topics);
     }
 }
