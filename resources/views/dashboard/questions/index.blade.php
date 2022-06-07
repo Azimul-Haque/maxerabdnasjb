6f4eff51f737511a29a95718ce21a6601c6eac4b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 17:10:50 +0600

    Commit Tue 06/07/2022 17:10:50.22 31916

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -8,150 +8,154 @@
 @section('content')
     @section('page-header') প্রশ্নব্যাংক @endsection
     <div class="container-fluid">
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">প্রশ্নব্যাংক</h3>
-
-            <div class="card-tools">
-                <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addQuesitonModal">
-                    <i class="fas fa-plus-circle"></i> নতুন প্রশ্ন যোগ
-                </button>
+        <div class="row">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">
+                      <h3 class="card-title">প্রশ্নব্যাংক</h3>
+          
+                      <div class="card-tools">
+                          <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addQuesitonModal">
+                              <i class="fas fa-plus-circle"></i> নতুন প্রশ্ন যোগ
+                          </button>
+                      </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body p-0">
+                      <table class="table">
+                          <thead>
+                              <tr>
+                                  <th>Question</th>
+                                  <th>Answer</th>
+                                  <th>Options</th>
+                                  <th>Action</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                          @foreach($questions as $question)
+                              <tr>
+                                  <td>
+                                      {{ $question->question }}<br/>
+                                      <span class="badge bg-success">{{ $question->topic->name }}</span>
+                                  </td>
+                                  <td>{{ $question->answer }}</td>
+                                  <td>{{ $question->opntion1 }}, {{ $question->opntion2 }}, {{ $question->opntion3 }}</td>
+                                  {{-- <td>
+                                      <div class="progress progress-xs">
+                                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                                      </div>
+                                  </td> --}}
+                              
+                                  <td align="right" width="40%">
+                                      <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editQuestionModal{{ $question->id }}">
+                                          <i class="fas fa-user-edit"></i>
+                                      </button>
+                                      {{-- Edit Question Modal Code --}}
+                                      {{-- Edit Question Modal Code --}}
+                                      <!-- Modal -->
+                                      <div class="modal fade" id="editQuestionModal{{ $question->id }}" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true" data-backdrop="static">
+                                          <div class="modal-dialog" role="document">
+                                          <div class="modal-content">
+                                              <div class="modal-header bg-primary">
+                                              <h5 class="modal-title" id="editUserModalLabel">প্রশ্ন হালনাগাদ</h5>
+                                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                  <span aria-hidden="true">&times;</span>
+                                              </button>
+                                              </div>
+                                              <form method="post" action="{{ route('dashboard.users.update', $question->id) }}">
+                                                  <div class="modal-body">
+                                                  
+                                                      @csrf
+          
+                                                      <div class="input-group mb-3">
+                                                          <input type="text"
+                                                                  name="name"
+                                                                  class="form-control"
+                                                                  value="{{ $question->name }}"
+                                                                  placeholder="নাম" required>
+                                                          <div class="input-group-append">
+                                                              <div class="input-group-text"><span class="fas fa-user"></span></div>
+                                                          </div>
+                                                      </div>
+          
+                                                      <div class="input-group mb-3">
+                                                          <input type="text"
+                                                                  name="mobile"
+                                                                  value="{{ $question->mobile }}"
+                                                                  autocomplete="off"
+                                                                  class="form-control"
+                                                                  placeholder="মোবাইল নম্বর (১১ ডিজিট)" required>
+                                                          <div class="input-group-append">
+                                                              <div class="input-group-text"><span class="fas fa-phone"></span></div>
+                                                          </div>
+                                                      </div>
+          
+                                                      {{-- <div class="input-group mb-3">
+                                                          <select name="role" class="form-control" required>
+                                                              <option disabled="" value="">ধরন নির্ধারণ করুন</option>
+                                                              <option value="admin" @if($question->role == 'admin') selected="" @endif>এডমিন</option>
+                                                              <option value="manager" @if($question->role == 'manager') selected="" @endif>ম্যানেজার</option>
+                                                              <option value="user" @if($question->role == 'user') selected="" @endif>ব্যবহারকারী</option>
+                                                          </select>
+                                                          <div class="input-group-append">
+                                                              <div class="input-group-text"><span class="fas fa-user-secret"></span></div>
+                                                          </div>
+                                                      </div> --}}
+                                                  </div>
+                                                  <div class="modal-footer">
+                                                  <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
+                                                  <button type="submit" class="btn btn-primary">দাখিল করুন</button>
+                                                  </div>
+                                              </form>
+                                          </div>
+                                          </div>
+                                      </div>
+                                      {{-- Edit Question Modal Code --}}
+                                      {{-- Edit Question Modal Code --}}
+          
+                                      <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteUserModal{{ $question->id }}">
+                                          <i class="fas fa-user-minus"></i>
+                                      </button>
+                                  </td>
+                                  {{-- Delete Question Modal Code --}}
+                                  {{-- Delete Question Modal Code --}}
+                                  <!-- Modal -->
+                                  <div class="modal fade" id="deleteUserModal{{ $question->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteUserModalLabel" aria-hidden="true" data-backdrop="static">
+                                      <div class="modal-dialog" role="document">
+                                      <div class="modal-content">
+                                          <div class="modal-header bg-danger">
+                                          <h5 class="modal-title" id="deleteUserModalLabel">ব্যবহারকারী ডিলেট</h5>
+                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                              <span aria-hidden="true">&times;</span>
+                                          </button>
+                                          </div>
+                                          <div class="modal-body">
+                                          আপনি কি নিশ্চিতভাবে এই ব্যবহারকারীকে ডিলেট করতে চান?<br/>
+                                          <center>
+                                              <big><b>{{ $question->name }}</b></big><br/>
+                                              <small><i class="fas fa-phone"></i> {{ $question->mobile }}</small>
+                                          </center>
+                                          </div>
+                                          <div class="modal-footer">
+                                          <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
+                                          <a href="{{ route('dashboard.users.delete', $question->id) }}" class="btn btn-danger">ডিলেট করুন</a>
+                                          </div>
+                                      </div>
+                                      </div>
+                                  </div>
+                                  {{-- Delete Question Modal Code --}}
+                                  {{-- Delete Question Modal Code --}}
+                              </tr>
+                          @endforeach
+                          </tbody>
+                      </table>
+                    </div>
+                    <!-- /.card-body -->
+                  </div>
+                  {{ $questions->links() }}
+              </div>
             </div>
-          </div>
-          <!-- /.card-header -->
-          <div class="card-body p-0">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Question</th>
-                        <th>Answer</th>
-                        <th>Options</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($questions as $question)
-                    <tr>
-                        <td>
-                            {{ $question->question }}<br/>
-                            <span class="badge bg-success">{{ $question->topic->name }}</span>
-                        </td>
-                        <td>{{ $question->answer }}</td>
-                        <td>{{ $question->opntion1 }}, {{ $question->opntion2 }}, {{ $question->opntion3 }}</td>
-                        {{-- <td>
-                            <div class="progress progress-xs">
-                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                            </div>
-                        </td> --}}
-                    
-                        <td align="right" width="40%">
-                            <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editQuestionModal{{ $question->id }}">
-                                <i class="fas fa-user-edit"></i>
-                            </button>
-                            {{-- Edit Question Modal Code --}}
-                            {{-- Edit Question Modal Code --}}
-                            <!-- Modal -->
-                            <div class="modal fade" id="editQuestionModal{{ $question->id }}" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true" data-backdrop="static">
-                                <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header bg-primary">
-                                    <h5 class="modal-title" id="editUserModalLabel">প্রশ্ন হালনাগাদ</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                    </div>
-                                    <form method="post" action="{{ route('dashboard.users.update', $question->id) }}">
-                                        <div class="modal-body">
-                                        
-                                            @csrf
-
-                                            <div class="input-group mb-3">
-                                                <input type="text"
-                                                        name="name"
-                                                        class="form-control"
-                                                        value="{{ $question->name }}"
-                                                        placeholder="নাম" required>
-                                                <div class="input-group-append">
-                                                    <div class="input-group-text"><span class="fas fa-user"></span></div>
-                                                </div>
-                                            </div>
-
-                                            <div class="input-group mb-3">
-                                                <input type="text"
-                                                        name="mobile"
-                                                        value="{{ $question->mobile }}"
-                                                        autocomplete="off"
-                                                        class="form-control"
-                                                        placeholder="মোবাইল নম্বর (১১ ডিজিট)" required>
-                                                <div class="input-group-append">
-                                                    <div class="input-group-text"><span class="fas fa-phone"></span></div>
-                                                </div>
-                                            </div>
-
-                                            {{-- <div class="input-group mb-3">
-                                                <select name="role" class="form-control" required>
-                                                    <option disabled="" value="">ধরন নির্ধারণ করুন</option>
-                                                    <option value="admin" @if($question->role == 'admin') selected="" @endif>এডমিন</option>
-                                                    <option value="manager" @if($question->role == 'manager') selected="" @endif>ম্যানেজার</option>
-                                                    <option value="user" @if($question->role == 'user') selected="" @endif>ব্যবহারকারী</option>
-                                                </select>
-                                                <div class="input-group-append">
-                                                    <div class="input-group-text"><span class="fas fa-user-secret"></span></div>
-                                                </div>
-                                            </div> --}}
-                                        </div>
-                                        <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                        <button type="submit" class="btn btn-primary">দাখিল করুন</button>
-                                        </div>
-                                    </form>
-                                </div>
-                                </div>
-                            </div>
-                            {{-- Edit Question Modal Code --}}
-                            {{-- Edit Question Modal Code --}}
-
-                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteUserModal{{ $question->id }}">
-                                <i class="fas fa-user-minus"></i>
-                            </button>
-                        </td>
-                        {{-- Delete Question Modal Code --}}
-                        {{-- Delete Question Modal Code --}}
-                        <!-- Modal -->
-                        <div class="modal fade" id="deleteUserModal{{ $question->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteUserModalLabel" aria-hidden="true" data-backdrop="static">
-                            <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header bg-danger">
-                                <h5 class="modal-title" id="deleteUserModalLabel">ব্যবহারকারী ডিলেট</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                                </div>
-                                <div class="modal-body">
-                                আপনি কি নিশ্চিতভাবে এই ব্যবহারকারীকে ডিলেট করতে চান?<br/>
-                                <center>
-                                    <big><b>{{ $question->name }}</b></big><br/>
-                                    <small><i class="fas fa-phone"></i> {{ $question->mobile }}</small>
-                                </center>
-                                </div>
-                                <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                <a href="{{ route('dashboard.users.delete', $question->id) }}" class="btn btn-danger">ডিলেট করুন</a>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                        {{-- Delete Question Modal Code --}}
-                        {{-- Delete Question Modal Code --}}
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-          </div>
-          <!-- /.card-body -->
         </div>
-        {{ $questions->links() }}
-    </div>
 
     {{-- Add Question Modal Code --}}
     {{-- Add Question Modal Code --}}
