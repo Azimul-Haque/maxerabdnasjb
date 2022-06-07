c23083089819d13612dcc919261929d88f035901
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 17:30:22 +0600

    Commit Tue 06/07/2022 17:30:22.26 24418

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -225,7 +225,7 @@
                                       {{-- Edit Topic Modal Code --}}
           
                                       <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteTopicModal{{ $topic->id }}">
-                                          <i class="fas fa-user-minus"></i>
+                                          <i class="far fa-trash-alt"></i>
                                       </button>
                                   </td>
                                   {{-- Delete Topic Modal Code --}}
