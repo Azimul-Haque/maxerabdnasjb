6fd0bd48ab713da32e90678a4120114733ba289c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 22:59:31 +0600

    Commit Tue 06/07/2022 22:59:31.25 18189

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -335,7 +335,7 @@
                                 <input type="file" id="image" name="image" accept="image/*">
                             </div>
                             <center>
-                                <img src="{{ asset('images/placeholder.png')}}" id='img-upload' style="height: 180px; width: auto;" class="img-responsive" />
+                                <img src="{{ asset('images/placeholder.png')}}" id='img-upload' style="width: 250px; height: auto;" class="img-responsive" />
                             </center>
                         </div>
                         <div class="col-md-6">
