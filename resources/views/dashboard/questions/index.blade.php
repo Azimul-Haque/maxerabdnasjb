b751267125667561cbcab324a320bcc4eaccd02a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 21:07:50 +0600

    Commit Tue 06/07/2022 21:07:50.31 31777

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -308,6 +308,17 @@
                                class="form-control"
                                placeholder="সঠিক উত্তর" required>
                         <div class="input-group-append">
+                            <div class="input-group-text"><span class="<i class="fal fa-clipboard-list"></i>"></span></div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text"
+                               name="answer"
+                               value="{{ old('answer') }}"
+                               autocomplete="off"
+                               class="form-control"
+                               placeholder="সঠিক উত্তর" required>
+                        <div class="input-group-append">
                             <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
                         </div>
                     </div>
