a0433138203ab5b1f3598aee02162cca63e5255a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-08 02:42:53 +0600

    Commit Wed 06/08/2022  2:42:53.22 15170

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -36,6 +36,7 @@
                                   <td>
                                       {{ $question->question }}<br/>
                                       <span class="badge bg-success">{{ $question->topic->name }}</span>
+                                      <span class="badge bg-info">{{ $question->topic->name }}</span>
                                   </td>
                                   <td>{{ $question->answer }}</td>
                                   <td>{{ $question->option1 }}, {{ $question->option2 }}, {{ $question->option3 }}</td>
