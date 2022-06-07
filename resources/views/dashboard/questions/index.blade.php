4c0d973b4a01264fc8da8982808c14f521ce6062
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-08 02:39:58 +0600

    Commit Wed 06/08/2022  2:39:58.24 20289

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -427,17 +427,17 @@
 
 @section('third_party_scripts')
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script>
+{{-- <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
-    ClassicEditor
-        .create( document.querySelector( '.summernote' ) )
-        .then( editor => {
-                console.log( editor );
-        } )
-        .catch( error => {
-                console.error( error );
-        } );
+    // ClassicEditor
+    //     .create( document.querySelector( '.summernote' ) )
+    //     .then( editor => {
+    //             console.log( editor );
+    //     } )
+    //     .catch( error => {
+    //             console.error( error );
+    //     } );
 </script>
 <script type="text/javascript">
 
