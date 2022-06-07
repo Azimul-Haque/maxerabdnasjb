6465c7e772998565e4fba22ae6979100d844c1d8
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-08 01:33:05 +0600

    Commit Wed 06/08/2022  1:33:05.30 19707

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -80,6 +80,8 @@ class QuestionController extends Controller
 
     public function storeQuestions(Request $request)
     {
+        ini_set('post_max_size', '20M');
+        ini_set('upload_max_filesize', '20M');
         dd($request->file('image'));
         $this->validate($request,array(
             'topic_id'    => 'required|string|max:191',
