7ce5cfe458c8c2aef66d1d1d378f84e446ad676e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 21:14:27 +0600

    Commit Tue 06/07/2022 21:14:27.23 26946

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -316,6 +316,17 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-md-6">
+                            <div class="input-group mb-3">
+                                <select name="difficulty" class="form-control" required>
+                                    <option selected="" disabled="" value="">সিলেক্ট করুন</option>
+                                    <option value="1">সহজ</option>
+                                </select>
+                                <div class="input-group-append">
+                                    <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="input-group mb-3">
                         <select name="role" id="adduserrole" class="form-control" required>
