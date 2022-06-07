2e6a35d41134ca7aa5bf14ca9e70062fea743b98
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-08 03:14:40 +0600

    Commit Wed 06/08/2022  3:14:40.28 19286

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -7,7 +7,7 @@
 		<div class="error-page">
 			<h2 class="headline text-warning"> 403</h2>
 			<div class="error-content">
-				<h3><i class="fas fa-exclamation-triangle text-warning"></i> You are not allowed.</h3>
+				<h3><i class="fas fa-exclamation-triangle text-warning"></i> You are NOT ALLOWED!</h3>
 
 				<p>
 					The page you are requesting is not allowed for you.
