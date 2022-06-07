a4c83d63b2a4802535367e231e5f499fb298c380
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-08 01:52:26 +0600

    Commit Wed 06/08/2022  1:52:26.28 28830

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -72,20 +72,20 @@
                                                           </div>
                                                       </div>
                                                       <div class="input-group mb-3">
-                                                          <input type="text" name="answer" value="{{ old('answer') }}" class="form-control" placeholder="সঠিক উত্তর" required>
+                                                          <input type="text" name="answer" value="{{ $question->answer }}" class="form-control" placeholder="সঠিক উত্তর" required>
                                                           <div class="input-group-append">
                                                               <div class="input-group-text"><span class="far fa-check-circle"></span></div>
                                                           </div>
                                                       </div>
                                                       <div class="row">
                                                           <div class="col-md-4">
-                                                              <input type="text" name="option1" value="{{ old('option1') }}" class="form-control mb-3" placeholder="অপশন ১" required>
+                                                              <input type="text" name="option1" value="{{ $question->option1 }}" class="form-control mb-3" placeholder="অপশন ১" required>
                                                           </div>
                                                           <div class="col-md-4">
-                                                              <input type="text" name="option2" value="{{ old('option2') }}" class="form-control mb-3" placeholder="অপশন ২" required>
+                                                              <input type="text" name="option2" value="{{ $question->option2 }}" class="form-control mb-3" placeholder="অপশন ২" required>
                                                           </div>
                                                           <div class="col-md-4">
-                                                              <input type="text" name="option3" value="{{ old('option3') }}" class="form-control mb-3" placeholder="অপশন ৩" required>
+                                                              <input type="text" name="option3" value="{{ $question->option3 }}" class="form-control mb-3" placeholder="অপশন ৩" required>
                                                           </div>
                                                       </div>
                                                       <div class="row">
