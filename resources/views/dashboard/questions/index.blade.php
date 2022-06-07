96d91ebf72af36fdb49e790a60f26ad7a7462b67
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-08 02:14:51 +0600

    Commit Wed 06/08/2022  2:14:51.22 28926

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -428,6 +428,7 @@
 
 @section('third_party_scripts')
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" src="{{ asset('vendor/summernote/summernote.min.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
 <script type="text/javascript">
