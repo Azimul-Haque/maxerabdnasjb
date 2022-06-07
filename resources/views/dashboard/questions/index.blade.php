5e294a275d89e641277a48d5104679d120fcb2e8
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-08 02:35:08 +0600

    Commit Wed 06/08/2022  2:35:08.29 20798

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -372,7 +372,7 @@
                         </div>
                         <div class="col-md-6">
                             <label for="explanation">ব্যাখ্যা (প্রয়োজনে)</label><br/>
-                            <textarea class="form-control summernote" name="explanation" id="summernote" placeholder="ব্যাখ্যা" style="width: 100%; height: 220px;"></textarea>
+                            <textarea class="form-control summernote" name="explanation" id="explanation" placeholder="ব্যাখ্যা" style="width: 100%; height: 220px;"></textarea>
                         </div>
                     </div>
               </div>
