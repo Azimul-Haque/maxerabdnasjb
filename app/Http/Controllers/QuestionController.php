3df864b8dc8735673b0add6c5ac7fe128fbd392f
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 23:09:55 +0600

    Commit Tue 06/07/2022 23:09:55.22 21848

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -86,7 +86,8 @@ class QuestionController extends Controller
             'option2'    => 'required|string|max:191',
             'option3'    => 'required|string|max:191',
             'difficulty' => 'required|string|max:191',
-            'difficulty' => 'required|string|max:191',
+            'image' => 'sometimes|string|max:191',
+            'image' => 'sometimes|string|max:191',
         ));
 
         $topic = new Topic;
