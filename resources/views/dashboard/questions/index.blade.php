15b0378b46467d3d768b82e5242ea6c8776b1ee9
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 17:27:19 +0600

    Commit Tue 06/07/2022 17:27:19.30 4889

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -235,7 +235,7 @@
                                       <div class="modal-dialog" role="document">
                                       <div class="modal-content">
                                           <div class="modal-header bg-danger">
-                                          <h5 class="modal-title" id="deleteTopicModalLabel">ব্যবহারকারী ডিলেট</h5>
+                                          <h5 class="modal-title" id="deleteTopicModalLabel">টপিক ডিলেট</h5>
                                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                               <span aria-hidden="true">&times;</span>
                                           </button>
@@ -351,7 +351,7 @@
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header bg-warning">
-          <h5 class="modal-title" id="addQuesitonModalLabel">নতুন ব্যবহারকারী যোগ</h5>
+          <h5 class="modal-title" id="addQuesitonModalLabel">নতুন টপিক যোগ</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
