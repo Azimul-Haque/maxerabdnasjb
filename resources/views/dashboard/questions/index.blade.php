5bea841b46a11e292c468a7c9ce4095eb7d98751
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 23:02:14 +0600

    Commit Tue 06/07/2022 23:02:14.34 27374

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -246,7 +246,7 @@
                                           </div>
                                           <div class="modal-footer">
                                           <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                          <a href="{{ route('dashboard.users.delete', $topic->id) }}" class="btn btn-danger">ডিলেট করুন</a>
+                                          <a href="{{ route('dashboard.topic.delete', $topic->id) }}" class="btn btn-danger">ডিলেট করুন</a>
                                           </div>
                                       </div>
                                       </div>
