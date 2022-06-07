3168ff11bcbbd7543a5036aa740fa45d07bed201
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 16:47:35 +0600

    Commit Tue 06/07/2022 16:47:35.24 29432

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Question;
+use App\Topic;
+
 class QuestionController extends Controller
 {
     public function __construct()
