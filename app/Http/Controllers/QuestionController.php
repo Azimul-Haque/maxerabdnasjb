163f37bdf48b3faf833c80f14509015ec51c5224
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-08 00:59:49 +0600

    Commit Wed 06/08/2022  0:59:49.23 19141

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -105,7 +105,7 @@ class QuestionController extends Controller
         // image upload
         if($request->hasFile('image')) {
             $image      = $request->file('image');
-            $filename   = random_string(5) . time() .'.' . "jpg";
+            $filename   = random_string(5) . time() .'.' . "webp";
             $location   = public_path('images/questions/'. $filename);
             Image::make($image)->resize(300, null, function ($constraint) { $constraint->aspectRatio(); })->save($location);
             // $question->questionimage->image = $filename;
