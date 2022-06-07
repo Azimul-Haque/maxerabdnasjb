651a58fe89ee622e4b4d94c9deeb8af38753ce36
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-07 22:57:14 +0600

    Commit Tue 06/07/2022 22:57:14.22 25100

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -394,7 +394,7 @@
 @endsection
 
 @section('third_party_scripts')
-    {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    {{-- {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
     <script type="text/javascript">
         $('#adduserrole').change(function () {
             if($('#adduserrole').val() == 'accountant') {
