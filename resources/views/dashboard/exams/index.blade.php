eca8d071fe14f2e28db2d7b502b5c1438d7ca681
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 02:27:06 +0600

    Commit Fri 06/10/2022  2:27:06.29 21761

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -227,7 +227,7 @@
                                           <div class="modal-dialog" role="document">
                                           <div class="modal-content">
                                               <div class="modal-header bg-warning">
-                                              <h5 class="modal-title" id="editTopicModalLabel">টপিক হালনাগাদ</h5>
+                                              <h5 class="modal-title" id="editTopicModalLabel">ক্যাটাগরি হালনাগাদ</h5>
                                               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                   <span aria-hidden="true">&times;</span>
                                               </button>
