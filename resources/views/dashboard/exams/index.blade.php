8cc85210545b1329071cba583c61a0ba6e7b24c5
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 03:25:38 +0600

    Commit Fri 06/10/2022  3:25:38.29 27372

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -4,6 +4,7 @@
 @section('third_party_stylesheets')
 <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap-datepicker.min.css') }}">
 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript" src="{{ asset('js/jquery-for-dp.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
@@ -479,7 +480,7 @@
 @endsection
 
 @section('third_party_scripts')
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+{{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
 
 {{-- <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
