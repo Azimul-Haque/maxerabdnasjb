544caa494dc00ce907e46c9140a209231affcd0b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 03:59:01 +0600

    Commit Fri 06/10/2022  3:59:01.25 5368

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -15,10 +15,10 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-9">
-            
+                A
             </div>
             <div class="col-md-3">
-                
+                B   
             </div>
         </div>
     </div>
