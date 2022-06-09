4bfee0922645f92178b0cf90bbe4472d3b35e974
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 02:49:21 +0600

    Commit Fri 06/10/2022  2:49:21.29 8081

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -389,7 +389,7 @@
                         </div>
                         <div class="col-md-6">
                             <div class="input-group mb-3">
-                                <input type="text" name="available_to" id="available_to" value="{{ old('available_to') }}" class="form-control" autocomplete="off" placeholder="চালু হবে" required>
+                                <input type="text" name="available_to" id="available_to" value="{{ old('available_to') }}" class="form-control" autocomplete="off" placeholder="চালু থাকবে (পর্যন্ত)" required>
                                 <div class="input-group-append">
                                     <div class="input-group-text"><span class="fas fa-percent"></span></div>
                                 </div>
