066dccda6debf8e3bfebc27d1fe6528fb5785b54
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 02:41:17 +0600

    Commit Fri 06/10/2022  2:41:17.35 11745

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -361,7 +361,7 @@
                         </div>
                         <div class="col-md-6">
                             <div class="input-group mb-3">
-                                <input type="number" name="negativepercentage" value="{{ old('negativepercentage') }}" min="0" class="form-control" placeholder="প্রতি প্রশ্নের মান" required>
+                                <input type="number" name="negativepercentage" value="{{ old('negativepercentage') }}" min="0" class="form-control" placeholder="নেগেটিভ মার্ক পারসেন্টেজ" required>
                                 <div class="input-group-append">
                                     <div class="input-group-text"><span class="fas fa-sort-amount-up-alt"></span></div>
                                 </div>
