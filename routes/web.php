7cac2e5dd9b9fd2139179651c39ae7b361aaf6cb
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 02:30:40 +0600

    Commit Fri 06/10/2022  2:30:40.23 16324

--- routes/web.php
+++ routes/web.php
@@ -43,9 +43,9 @@ Route::post('/dashboard/exams/store', 'ExamController@storeExam')->name('dashboa
 Route::post('/dashboard/exams/{id}/update', 'ExamController@updateExam')->name('dashboard.exams.update');
 Route::get('/dashboard/exams/{id}/delete', 'ExamController@deleteExam')->name('dashboard.exams.delete');
 
-Route::post('/dashboard/exams/topic/store', 'ExamController@storeQuestionsTopic')->name('dashboard.questions.topic.store');
-Route::post('/dashboard/exams/topic/{id}/update', 'ExamController@updateQuestionsTopic')->name('dashboard.questions.topic.update');
-Route::get('/dashboard/exams/topic/{id}/delete', 'ExamController@deleteQuestionsTopic')->name('dashboard.questions.topic.delete');
+Route::post('/dashboard/exams/topic/store', 'ExamController@storeQuestionsTopic')->name('dashboard.exams.category.store');
+Route::post('/dashboard/exams/topic/{id}/update', 'ExamController@updateQuestionsTopic')->name('dashboard.exams.category.update');
+Route::get('/dashboard/exams/topic/{id}/delete', 'ExamController@deleteQuestionsTopic')->name('dashboard.exams.category.delete');
 // Route::get('/dashboard/balance', 'DashboardController@getBalance')->name('dashboard.balance');
 // Route::post('/dashboard/balance/store', 'DashboardController@storeBalance')->name('dashboard.balance.store');
 // Route::get('/dashboard/balance/{id}/delete', 'DashboardController@deleteBalance')->name('dashboard.balance.delete');
