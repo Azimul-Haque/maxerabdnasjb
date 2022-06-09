504923f6235e580e4f677c4a7b6c808a1a5575f7
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 02:34:24 +0600

    Commit Fri 06/10/2022  2:34:24.29 11914

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -399,7 +399,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header bg-warning">
-          <h5 class="modal-title" id="addQuesitonModalLabel">নতুন টপিক যোগ</h5>
+          <h5 class="modal-title" id="addQuesitonModalLabel">নতুন ক্যাটাগরি যোগ</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
