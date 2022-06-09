6e1d1f0298d4294c2a10e0bc73ae18cae015e38b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 01:30:51 +0600

    Commit Fri 06/10/2022  1:30:51.26 13988

--- database/migrations/2022_06_10_011814_create_examcategories_table.php
+++ database/migrations/2022_06_10_011814_create_examcategories_table.php
@@ -15,7 +15,7 @@ class CreateExamcategoriesTable extends Migration
     {
         Schema::create('examcategories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
