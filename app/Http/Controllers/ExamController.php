77b32d472618d776f2aa1c5d812b5c25b62437c7
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 02:29:01 +0600

    Commit Fri 06/10/2022  2:29:01.24 27040

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -38,4 +38,166 @@ class ExamController extends Controller
                     ->withExams($exams)
                     ->withExamcategories($examcategories);
     }
+
+    public function storeQuestionsTopic(Request $request)
+    {
+        $this->validate($request,array(
+            'name'        => 'required|string|max:191',
+        ));
+
+        $topic = new Topic;
+        $topic->name = $request->name;
+        $topic->save();
+
+        Session::flash('success', 'Topic created successfully!');
+        return redirect()->route('dashboard.questions');
+    }
+
+    public function updateQuestionsTopic(Request $request, $id)
+    {
+        $this->validate($request,array(
+            'name' => 'required|string|max:191',
+        ));
+
+        $topic = Topic::find($id);;
+        $topic->name = $request->name;
+        $topic->save();
+
+        Session::flash('success', 'Topic updated successfully!');
+        return redirect()->route('dashboard.questions');
+    }
+
+    public function deleteQuestionsTopic($id)
+    {
+        $topic = Topic::find($id);
+        $topic->delete();
+
+        Session::flash('success', 'Topic deleted successfully!');
+        return redirect()->route('dashboard.questions');
+    }
+
+    public function storeQuestion(Request $request)
+    {
+        // dd($request->file('image'));
+        $this->validate($request,array(
+            'topic_id'    => 'required|string|max:191',
+            'question'    => 'required|string|max:191',
+            'answer'      => 'required|string|max:191',
+            'option1'     => 'required|string|max:191',
+            'option2'     => 'required|string|max:191',
+            'option3'     => 'required|string|max:191',
+            'difficulty'  => 'required|string|max:191',
+            'image'       => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg,webp|max:3000',
+            'explanation' => 'sometimes|max:2048',
+        ));
+
+        $question = new Question;
+        $question->topic_id = $request->topic_id;
+        $question->question = $request->question;
+        $question->answer = $request->answer;
+        $question->option1 = $request->option1;
+        $question->option2 = $request->option2;
+        $question->option3 = $request->option3;
+        $question->difficulty = $request->difficulty;
+        $question->save();
+
+        // image upload
+        if($request->hasFile('image')) {
+            $image      = $request->file('image');
+            $filename   = random_string(5) . time() .'.' . "webp";
+            $location   = public_path('images/questions/'. $filename);
+            Image::make($image)->resize(350, null, function ($constraint) { $constraint->aspectRatio(); })->save($location);
+            $questionimage = new Questionimage;
+            $questionimage->question_id = $question->id;
+            $questionimage->image = $filename;
+            $questionimage->save();
+        }
+
+        if($request->explanation) {
+            $questionexplanation = new Questionexplanation;
+            $questionexplanation->question_id = $question->id;
+            $questionexplanation->explanation = $request->explanation;
+            $questionexplanation->save();
+        }
+
+        Session::flash('success', 'Question created successfully!');
+        return redirect()->route('dashboard.questions');
+    }
+
+    public function updateQuestion(Request $request, $id)
+    {
+        // dd($request->file('image'));
+        $this->validate($request,array(
+            'topic_id'    => 'required|string|max:191',
+            'question'    => 'required|string|max:191',
+            'answer'      => 'required|string|max:191',
+            'option1'     => 'required|string|max:191',
+            'option2'     => 'required|string|max:191',
+            'option3'     => 'required|string|max:191',
+            'difficulty'  => 'required|string|max:191',
+            'image'       => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg,webp|max:3000',
+            'explanation' => 'sometimes|max:2048',
+        ));
+
+        $question = Question::findOrFail($id);
+        $question->topic_id = $request->topic_id;
+        $question->question = $request->question;
+        $question->answer = $request->answer;
+        $question->option1 = $request->option1;
+        $question->option2 = $request->option2;
+        $question->option3 = $request->option3;
+        $question->difficulty = $request->difficulty;
+        $question->save();
+
+        // image upload
+        if($request->hasFile('image')) {
+            if($question->questionimage) {
+                $image_path = public_path('images/questions/'. $question->questionimage->image);
+                if(File::exists($image_path)) {
+                    File::delete($image_path);
+                }
+                $question->questionimage->delete();
+            }
+            $image      = $request->file('image');
+            $filename   = random_string(5) . time() .'.' . "webp";
+            $location   = public_path('images/questions/'. $filename);
+            Image::make($image)->resize(350, null, function ($constraint) { $constraint->aspectRatio(); })->save($location);
+            $questionimage = new Questionimage;
+            $questionimage->question_id = $question->id;
+            $questionimage->image = $filename;
+            $questionimage->save();
+        }
+
+        if($request->explanation) {
+            if($question->questionexplanation) {
+                $question->questionexplanation->explanation = $request->explanation;
+                $question->questionexplanation->save();
+            } else {
+                $questionexplanation = new Questionexplanation;
+                $questionexplanation->question_id = $question->id;
+                $questionexplanation->explanation = $request->explanation;
+                $questionexplanation->save();
+            }
+        }
+
+        Session::flash('success', 'Question updated successfully!');
+        return redirect()->route('dashboard.questions');
+    }
+
+    public function deleteQuestion($id)
+    {
+        $question = Question::find($id);
+        if($question->questionimage) {
+            $image_path = public_path('images/questions/'. $question->questionimage->image);
+            if(File::exists($image_path)) {
+                File::delete($image_path);
+            }
+            $question->questionimage->delete();
+        }
+        $question->questionexplanation->delete();
+        $question->delete();
+
+        Session::flash('success', 'Question deleted successfully!');
+        return redirect()->route('dashboard.questions');
+    }
 }
