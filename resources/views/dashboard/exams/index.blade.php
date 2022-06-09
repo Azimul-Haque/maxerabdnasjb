34197416feb7e6ed5647d484b59619a28c9d2d12
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 02:38:47 +0600

    Commit Fri 06/10/2022  2:38:47.23 28652

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -337,7 +337,7 @@
                         </div>
                         <div class="col-md-6">
                             <div class="input-group mb-3">
-                                <input type="number" name="duration" value="{{ old('duration') }}" min="1" class="form-control" placeholder="সময় (ডিউরেশন, মিনিটে)" required>
+                                <input type="number" name="duration" value="{{ old('duration') }}" min="1" class="form-control" placeholder="সময়কাল (মিনিটে)" required>
                                 <div class="input-group-append">
                                     <div class="input-group-text"><span class="fas fa-stopwatch"></span></div>
                                 </div>
