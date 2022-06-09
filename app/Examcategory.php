59bf9c2bf02d9baf36ec01178c9510c8ad244f45
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-10 01:31:29 +0600

    Commit Fri 06/10/2022  1:31:29.21 14568

--- app/Examcategory.php
+++ app/Examcategory.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Examcategory extends Model
 {
     public $timestamps = false;
+
+    public function exams(){
+        return $this->hasMany('App\Exam');
+    }
 }
