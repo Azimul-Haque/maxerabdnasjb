2878e7380a3835ae7b5c0fa0a1be2ff4bc682ec8
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 02:51:01 +0600

    Commit Sat 06/11/2022  2:51:01.24 26906

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -164,7 +164,7 @@ class ExamController extends Controller
     public function storeExamQuestion(Request $request)
     {
         $this->validate($request,array(
-            'name'        => 'required|string|max:191',
+            'questioncheck'        => 'required',
         ));
 
         $category = new Examcategory;
