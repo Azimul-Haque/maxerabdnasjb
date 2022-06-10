3e7ad4ec21942fdb4ef22f8ee52c1c49ab1157b8
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 01:37:47 +0600

    Commit Sat 06/11/2022  1:37:47.25 6684

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -135,7 +135,7 @@
                     </div>
                     <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                    <button type="submit" class="btn btn-warning">দাখিল করুন</button>
+                    <button type="submit" class="btn btn-success">দাখিল করুন</button>
                     </div>
                 </form>
             </div>
