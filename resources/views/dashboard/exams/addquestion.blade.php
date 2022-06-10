b99e0f2aee9ecf0daed6080b0b34dc8db4f93b80
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 02:40:36 +0600

    Commit Sat 06/11/2022  2:40:36.28 20306

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -4,6 +4,7 @@
 @section('third_party_stylesheets')
 <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
 @endsection
 
 @section('content')
