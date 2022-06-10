bc8a43f202ac71ba4d3ca3abc26fc4432a84bd57
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 02:33:59 +0600

    Commit Sat 06/11/2022  2:33:59.26 10837

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -157,8 +157,7 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
       $("#datatablemodal").DataTable({
-        "responsive": true, "lengthChange": false, "autoWidth": true,
-        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        
       });
     //   $('#datatablemodal222').DataTable({
     //     "paging": true,
