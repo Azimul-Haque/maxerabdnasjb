7e4b435c6e0b0bf344a1e8f2b52cbf8c6867b8c7
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 00:38:11 +0600

    Commit Sat 06/11/2022  0:38:11.28 4105

--- app/Exam.php
+++ app/Exam.php
@@ -9,4 +9,8 @@ class Exam extends Model
     public function examcategory(){
         return $this->belongsTo('App\Examcategory');
     }
+
+    public function exams(){
+        return $this->hasMany('App\Exam');
+    }
 }
