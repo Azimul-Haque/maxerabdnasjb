8c6eeb0e2e540fd9cb38a93e74c8805d50c4e4f2
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 02:05:40 +0600

    Commit Sat 06/11/2022  2:05:40.31 27152

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -152,7 +152,7 @@
 @endsection
 
 @section('third_party_scripts')
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
