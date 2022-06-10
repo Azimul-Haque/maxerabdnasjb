d6de1563376940af5e444bcacdaf0f2e9d631f82
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 01:54:51 +0600

    Commit Sat 06/11/2022  1:54:51.28 22026

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -2,6 +2,7 @@
 @section('title') ড্যাশবোর্ড | পরীক্ষা | {{ $exam->name }}@endsection
 
 @section('third_party_stylesheets')
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
 @endsection
 
