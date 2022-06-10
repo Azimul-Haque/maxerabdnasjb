2fc9da6d01df6758b0ebaff6407db676edea12cb
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 03:19:38 +0600

    Commit Sat 06/11/2022  3:19:38.27 19163

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -171,7 +171,9 @@
     });
     
     function checkboxquestion(id) {
-        var arr = $('#hiddencheckarray').val();
+        var arr = $('#hiddencheckarray').map(function () {
+            return this.value; // $(this)
+        }).get();
         console.log(arr);
         arr.push(id);
         $('#hiddencheckarray').val(arr);
