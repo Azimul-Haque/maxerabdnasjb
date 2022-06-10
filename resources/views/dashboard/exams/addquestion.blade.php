6f4249afe3071cce96026aeab0f45d726a62454e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 01:49:53 +0600

    Commit Sat 06/11/2022  1:49:53.22 8898

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -2,12 +2,7 @@
 @section('title') ড্যাশবোর্ড | পরীক্ষা | {{ $exam->name }}@endsection
 
 @section('third_party_stylesheets')
-<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap-datepicker.min.css') }}">
-
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script type="text/javascript" src="{{ asset('js/jquery-for-dp.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
 @endsection
 
 @section('content')
@@ -122,7 +117,7 @@
                 <form method="post" action="{{ route('dashboard.exams.category.store') }}">
                     <div class="modal-body">
                         @csrf
-                        <table class="table">
+                        <table class="table" id="datatable">
                             <thead>
                                 <tr>
                                     <th>#</th>
@@ -156,78 +151,24 @@
 @endsection
 
 @section('third_party_scripts')
-{{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
-
-{{-- <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script> --}}
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
-    $(document).ready(function(){
-        $('[data-toggle="tooltip"]').tooltip();
-        $("[rel='tooltip']").tooltip();
-    });
-    $("#available_from").datepicker({
-      format: 'MM dd, yyyy',
-      todayHighlight: true,
-      autoclose: true,
-      container:'#addExamModal',
-    });
-    $("#available_to").datepicker({
-      format: 'MM dd, yyyy',
-      todayHighlight: true,
-      autoclose: true,
-      container:'#addExamModal',
-    });
-    // ClassicEditor
-    //     .create( document.querySelector( '.summernote' ) )
-    //     .then( editor => {
-    //             console.log( editor );
-    //     } )
-    //     .catch( error => {
-    //             console.error( error );
-    //     } );
-</script>
-<script type="text/javascript">
-    $(document).ready( function() {
-      $(document).on('change', '.btn-file :file', function() {
-        var input = $(this),
-            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-        input.trigger('fileselect', [label]);
+    $(function () {
+      $("#example1").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
       });
+    });
+  </script>
 
-      $('.btn-file :file').on('fileselect', function(event, label) {
-          var input = $(this).parents('.input-group').find(':text'),
-              log = label;
-          if( input.length ) {
-              input.val(log);
-          } else {
-              if( log ) alert(log);
-          }
-      });
-      function readURL(input) {
-          if (input.files && input.files[0]) {
-              var reader = new FileReader();
-              reader.onload = function (e) {
-                  $('#img-upload').attr('src', e.target.result);
-              }
-              reader.readAsDataURL(input.files[0]);
-          }
-      }
-      $("#image").change(function(){
-          readURL(this);
-          var filesize = parseInt((this.files[0].size)/1024);
-          if(filesize > 10000) {
-            $("#image").val('');
-            // toastr.warning('File size is: '+filesize+' Kb. try uploading less than 300Kb', 'WARNING').css('width', '400px;');
-            Toast.fire({
-                icon: 'warning',
-                title: 'File size is: '+filesize+' Kb. try uploading less than 300Kb'
-            })
-            setTimeout(function() {
-            $("#img-upload").attr('src', '{{ asset('images/placeholder.png') }}');
-            }, 1000);
-          }
-      });
-
-    });
-</script>
 @endsection
