3ce0a1c17ab161acbce748f14fd7b848143b15f9
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 02:08:18 +0600

    Commit Sat 06/11/2022  2:08:18.21 25105

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -2,7 +2,7 @@
 @section('title') ড্যাশবোর্ড | পরীক্ষা | {{ $exam->name }}@endsection
 
 @section('third_party_stylesheets')
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
 @endsection
 
@@ -153,7 +153,7 @@
 
 @section('third_party_scripts')
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
     $(document).ready(function () {
