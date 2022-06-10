178e47ec0b6573f6f1016a988af6bbb307122000
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 01:37:21 +0600

    Commit Sat 06/11/2022  1:37:21.24 20512

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -113,7 +113,7 @@
     <div class="modal fade" id="addExamQuestionModal" tabindex="-1" role="dialog" aria-labelledby="addExamQuestionModalLabel" aria-hidden="true" data-backdrop="static">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
-                <div class="modal-header bg-info">
+                <div class="modal-header bg-successs">
                 <h5 class="modal-title" id="addExamQuestionModalLabel">নতুন ক্যাটাগরি যোগ</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
