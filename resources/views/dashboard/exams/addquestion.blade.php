bde0569e9cb4b2890d3d75f7948a09c9c360a20d
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 02:17:52 +0600

    Commit Sat 06/11/2022  2:17:52.27 28611

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -162,7 +162,7 @@
 <script type="text/javascript" src="https://adminlte.io/themes/v3/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
 <script type="text/javascript" src="https://adminlte.io/themes/v3/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
 <script type="text/javascript" src="https://adminlte.io/themes/v3/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script type="text/javascript" src="https://adminlte.io/themes/v3/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script type="text/javascript" src="https://adminlte.io/themes/v3/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
     $(document).ready( function () {
