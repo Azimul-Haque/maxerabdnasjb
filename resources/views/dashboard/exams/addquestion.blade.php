44aebc01b3c8109a733c80eb7f6730aed33f5b82
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-11 01:22:07 +0600

    Commit Sat 06/11/2022  1:22:07.29 27858

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -14,10 +14,13 @@
     @section('page-header') {{ $exam->name }} @endsection
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-9">
+            <div class="col-md-4">
                 A
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
+                B   
+            </div>
+            <div class="col-md-4">
                 B   
             </div>
         </div>
