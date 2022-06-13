6b6b69ac9d971bb70dc75a66b0b54137e7ad5ffd
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-14 01:43:58 +0600

    Commit Tue 06/14/2022  1:43:58.29 27388

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -225,7 +225,7 @@
                                       {{-- Edit Exam Modal Code --}}
                                       {{-- Edit Exam Modal Code --}}
           
-                                      <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteQuestionModal{{ $exam->id }}" disabled>
+                                      <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteQuestionModal{{ $exam->id }}" >
                                           <i class="far fa-trash-alt"></i>
                                       </button>
                                   </td>
