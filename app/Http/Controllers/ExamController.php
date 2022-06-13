d5589f643fdb1b7d0e30b67db0d07629103ee55a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-14 01:37:44 +0600

    Commit Tue 06/14/2022  1:37:44.23 12232

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -143,14 +143,7 @@ class ExamController extends Controller
         // dd($request->file('image'));
         // dd($request->file('image'));
         $this->validate($request,array(
-            'examcategory_id'    => 'required|string|max:191',
             'name'               => 'required|string|max:191',
-            'duration'           => 'required|string|max:191',
-            'qsweight'           => 'required|string|max:191',
-            'negativepercentage' => 'required|string|max:191',
-            'price_type'         => 'required|string|max:191',
-            'available_from'     => 'required|string|max:191',
-            'available_to'       => 'required|string|max:191',
         ));
 
         $exam = Exam::find($id);
