71ee6bf0e197f67fa616922f45a2f8a26b85253d
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-14 02:24:54 +0600

    Commit Tue 06/14/2022  2:24:54.27 16850

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -211,7 +211,7 @@ class ExamController extends Controller
             }
         }
         $hiddencheckarray = explode(',', $request->hiddencheckarray);
-        dd($hiddencheckarray);
+        dd(sort($hiddencheckarray));
         foreach($hiddencheckarray as $question_id) {
             $examquestion = new Examquestion;
             $examquestion->exam_id = $request->exam_id;
