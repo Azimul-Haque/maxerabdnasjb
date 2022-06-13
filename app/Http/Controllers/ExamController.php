0582122381a14ee4d599ad87f62f5d071ddcb62b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-14 00:30:21 +0600

    Commit Tue 06/14/2022  0:30:21.33 3735

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -209,7 +209,7 @@ class ExamController extends Controller
         $topics = Topic::all();
         foreach($topics as $topic) {
             $topicname = 'topic' . $topic->id;
-            if($request[$topicname] > 0) {
+            if($request[$topicname] == ) {
                 dd($request[$topicname]);
                 $topicquestions = Question::where('topic_id', $request[$topicname])->get();
                 dd($topicquestions);
