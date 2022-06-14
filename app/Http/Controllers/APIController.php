7ac1b496f11bfce5d4c45514ea4c297a6767ae61
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-15 03:11:17 +0600

    Commit Wed 06/15/2022  3:11:17.29 21959

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -13,6 +13,20 @@ class APIController extends Controller
 
     public function addUser(Request $request)
     {
-         dd('name');
+        $this->validate($request,array(
+            'name'        => 'required|max:255',
+            'email'       => 'required|max:255',
+            'message'     => 'required|max:255'
+        ));
+
+        $message = new Charioteermessage;
+        $message->name = $request->name;
+        $message->email = $request->email;
+        $message->message = $request->message;
+        $message->save();
+
+        return response()->json([
+            'success' => true
+        ]);
     }
 }
