b889c9daaaa0a2760ff4e4372c06367214aced7b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-15 02:52:44 +0600

    Commit Wed 06/15/2022  2:52:44.30 281

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('uid');
             $table->string('name');
             $table->string('role')->default('user');
             $table->string('mobile')->unique();
