e16a2307265cb1e17924740a76317acc5638d051
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-15 02:27:43 +0600

    Commit Wed 06/15/2022  2:27:43.23 27437

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class APIController extends Controller
 {
-    //
+    public function getExams()
+    {
+        if(!(Auth::user()->role == 'admin' || Auth::user()->role == 'manager')) {
+            abort(403, 'Access Denied');
+        }
+        
+        $exams = Exam::paginate(10);
+        $examcategories = Examcategory::all();
+
+        return view('dashboard.exams.index')
+                    ->withExams($exams)
+                    ->withExamcategories($examcategories);
+    }
 }
