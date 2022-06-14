606f021a797a1b56df52ed5787aabce86132bd5b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-15 02:27:30 +0600

    Commit Wed 06/15/2022  2:27:30.29 27557

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/testapi', 'DashboardController@index')->name('dashboard.index');
+Route::get('/testapi', 'APIController@test')->name('api.test');
