bd19b453368c9c9d53c4eb6087edadc50247a005
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-16 03:26:33 +0600

    Commit Thu 06/16/2022  3:26:33.25 18022

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -61,4 +61,34 @@ class APIController extends Controller
         ]);
         
     }
+    
+    public function addUser(Request $request)
+    {
+        $this->validate($request,array(
+            'uid'         => 'required|max:255|unique:users,uid',
+            'name'        => 'required|max:255',
+            'mobile'      => 'required|max:255',
+            'softtoken'   => 'required|max:255'
+        ));
+
+        if($request->softtoken == 'Rifat.Admin.2022')
+        {
+            $user = new User;
+            $user->uid = $request->uid;
+            $user->name = $request->name;
+            $user->role = 'user';
+            $user->mobile = substr($request->mobile, -11);
+            $user->password = Hash::make('12345678');
+            $user->save();
+
+            return response()->json([
+                'success' => true
+            ]);
+        }
+
+        return response()->json([
+            'success' => false
+        ]);
+        
+    }
 }
