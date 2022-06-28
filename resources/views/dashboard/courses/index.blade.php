7255cc3d50d63986282d258fcea023e631c119c5
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 01:11:04 +0600

    Commit Wed 06/29/2022  1:11:04.25 29594

--- resources/views/dashboard/courses/index.blade.php
+++ resources/views/dashboard/courses/index.blade.php
@@ -12,7 +12,7 @@
             <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">
-                      <h3 class="card-title">কোর্সসমূহ (মোটঃ {{ $totalquestions }})</h3>
+                      <h3 class="card-title">কোর্সসমূহ (মোটঃ {{ $totalcourses }})</h3>
           
                       <div class="card-tools">
                           <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addQuesitonModal">
