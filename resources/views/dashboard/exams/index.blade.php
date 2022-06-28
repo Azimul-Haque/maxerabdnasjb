970a16832ca8facb7ac29b47ee3c1f59c8a284b3
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 03:27:04 +0600

    Commit Wed 06/29/2022  3:27:04.23 28412

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -540,10 +540,6 @@
 {{-- <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
-    $(document).ready(function(){
-        $('[data-toggle="tooltip"]').tooltip();
-        $("[rel='tooltip']").tooltip();
-    });
     $("#available_from").datepicker({
       format: 'MM dd, yyyy',
       todayHighlight: true,
