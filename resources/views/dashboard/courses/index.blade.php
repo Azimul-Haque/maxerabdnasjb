8ae1c5db1a467077ef2b57f39a0a417b0fbdaffc
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 01:46:25 +0600

    Commit Wed 06/29/2022  1:46:25.26 16163

--- resources/views/dashboard/courses/index.blade.php
+++ resources/views/dashboard/courses/index.blade.php
@@ -85,7 +85,7 @@
                                       <div class="modal-dialog" role="document">
                                       <div class="modal-content">
                                           <div class="modal-header bg-danger">
-                                          <h5 class="modal-title" id="deleteQuestionModalLabel">প্রশ্ন ডিলেট</h5>
+                                          <h5 class="modal-title" id="deleteQuestionModalLabel">কোর্স ডিলেট</h5>
                                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                               <span aria-hidden="true">&times;</span>
                                           </button>
