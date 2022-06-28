58f780b5997d02500d6551f0214d1250b6ad97fd
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 02:02:43 +0600

    Commit Wed 06/29/2022  2:02:43.25 25036

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,5 @@ Route::get('/testapi', 'APIController@test')->name('api.test');
 Route::get('/checkuid/{softtoken}/{uid}', 'APIController@checkUid')->name('api.checkuid');
 Route::post('/adduser', 'APIController@addUser')->name('api.adduser');
 Route::post('/updateuser', 'APIController@updateUser')->name('api.updateuser');
+
+Route::get('/getcourses/{softtoken}', 'APIController@getCourses')->name('api.checkuid');
