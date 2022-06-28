ca94ecebf8457fbd34c7eec84312688c87f1535c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 00:48:39 +0600

    Commit Wed 06/29/2022  0:48:39.28 172

--- app/Courseexam.php
+++ app/Courseexam.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Courseexam extends Model
 {
-    //
+    public function examquestions(){
+        return $this->hasMany('App\Examquestion');
+    }
 }
