70abbd4644b27b47b4e91685331cdb6e25c93a0e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 03:39:37 +0600

    Commit Wed 06/29/2022  3:39:37.28 15271

--- resources/views/dashboard/courses/addexams.blade.php
+++ resources/views/dashboard/courses/addexams.blade.php
@@ -89,16 +89,16 @@
                                 <tr>
                                     <td>
                                         <div class="icheck-primary icheck-inline" style="float: left;">
-                                            <input type="checkbox" onchange="checkboxquestion({{ $question->id }})" id="check{{ $question->id }}" name="questioncheck[]" value="{{ $question->id }}" 
-                                            @if(in_array($question->id, $courseexamidarray)) checked="" @endif
+                                            <input type="checkbox" onchange="checkboxquestion({{ $exam->id }})" id="check{{ $exam->id }}" name="examcheck[]" value="{{ $exam->id }}" 
+                                            @if(in_array($exam->id, $courseexamidarray)) checked="" @endif
                                             />
-                                            <label for="check{{ $question->id }}"> </label>
+                                            <label for="check{{ $exam->id }}"> </label>
                                         </div>
                                     </td>
                                     <td>
-                                        {{ $question->question }}
+                                        {{ $exam->exam }}
                                     </td>
-                                    <td><span class="badge bg-success">{{ $question->topic->name }}</span></td>
+                                    <td><span class="badge bg-success">{{ $exam->topic->name }}</span></td>
                                 </tr>
                                 @endforeach
                             </tbody>
