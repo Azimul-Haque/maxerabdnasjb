227d3aeae4c8c2e7f4dfb1efd6be5326fb29ff23
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 01:50:31 +0600

    Commit Wed 06/29/2022  1:50:31.31 4167

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -168,7 +168,6 @@
                                 @foreach ($questions as $question)
                                 <tr>
                                     <td>
-                                        
                                         <div class="icheck-primary icheck-inline" style="float: left;">
                                             <input type="checkbox" onchange="checkboxquestion({{ $question->id }})" id="check{{ $question->id }}" name="questioncheck[]" value="{{ $question->id }}" 
                                             @if(in_array($question->id, $examquestionidarray)) checked="" @endif
