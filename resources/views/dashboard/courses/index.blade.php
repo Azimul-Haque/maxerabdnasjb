4c0d2bd3ab21349bee9f6484cc23050f60a53856
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 01:39:31 +0600

    Commit Wed 06/29/2022  1:39:31.28 24295

--- resources/views/dashboard/courses/index.blade.php
+++ resources/views/dashboard/courses/index.blade.php
@@ -253,7 +253,7 @@
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header bg-success">
-            <h5 class="modal-title" id="addCourseModalLabel">নতুন প্রশ্ন যোগ</h5>
+            <h5 class="modal-title" id="addCourseModalLabel">নতুন কোর্স যোগ</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
