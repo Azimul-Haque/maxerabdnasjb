e5d79f0706961ba6a9ec00a1073a1a900d7e00a3
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 01:06:03 +0600

    Commit Wed 06/29/2022  1:06:03.24 31679

--- app/course.php
+++ app/course.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    public function exams(){
-        return $this->hasMany('App\Exam');
+    public function Courseexams(){
+        return $this->hasMany('App\Courseexam');
     }
 }
