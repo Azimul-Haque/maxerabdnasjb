2bf0596e843a46ce168373400157fdaf4ee5bd0f
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 01:52:39 +0600

    Commit Wed 06/29/2022  1:52:39.27 4309

--- resources/views/dashboard/courses/index.blade.php
+++ resources/views/dashboard/courses/index.blade.php
@@ -152,8 +152,8 @@
                     <div class="input-group mb-3">
                         <select name="status" class="form-control" required>
                             <option selected="" disabled="" value="">স্ট্যাটাস</option>
-                            <option value="1" @if($course->status == 1) selected @endif>Active</option>
-                            <option value="0" @if($course->status == 0) selected @endif>In-active</option>
+                            <option value="1">Active</option>
+                            <option value="0">In-active</option>
                         </select>
                         <div class="input-group-append">
                             <div class="input-group-text"><span class="fas fa-star-half-alt"></span></div>
