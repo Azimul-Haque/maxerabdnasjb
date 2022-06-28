58522f428239729aa7a1b32c08ca09b982078750
Azimul-Haque <orbachinujbuk@gmail.com>
2022-06-29 01:53:40 +0600

    Commit Wed 06/29/2022  1:53:40.27 5374

--- database/migrations/2022_06_29_004217_create_courses_table.php
+++ database/migrations/2022_06_29_004217_create_courses_table.php
@@ -16,7 +16,7 @@ class CreateCoursesTable extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('status');
+            $table->integer('status')->default(0);
             $table->timestamps();
         });
     }
