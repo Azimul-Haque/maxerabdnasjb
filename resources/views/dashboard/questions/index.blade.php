782ff9ed1ac0262d5abc63b77c19c212585a786d
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 03:01:13 +0600

    Commit Wed 07/20/2022  3:01:13.25 23185

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -8,12 +8,6 @@
 <script src="{{ asset('js/select2.full.min.js') }}"></script>
 <style type="text/css">
   .select2-selection__choice{
-      margin-top: 0px!important;
-      padding-right: 5px!important;
-      padding-left: 5px!important;
-      background-color: transparent!important;
-      border:none!important;
-      border-radius: 4px!important;
       background-color: rgba(0, 139, 139, 0.09) !important;
   }
 </style>
