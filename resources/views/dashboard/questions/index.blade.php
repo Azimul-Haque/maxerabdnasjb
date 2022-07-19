4e43650eb518f07a555d86ec80e83b633b703f10
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 02:41:55 +0600

    Commit Wed 07/20/2022  2:41:55.26 7680

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -517,7 +517,6 @@
 <script>
     $('.multiple-select').select2({
       theme: 'bootstrap4',
-      placeholder: "ট্যাগ",
     });
     // ClassicEditor
     //     .create( document.querySelector( '.summernote' ) )
