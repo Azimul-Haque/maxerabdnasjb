135b1b35c8be9ce35ed6acf26222e9fc9c660bd1
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 02:57:38 +0600

    Commit Wed 07/20/2022  2:57:38.28 9750

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -86,7 +86,7 @@ class QuestionController extends Controller
     public function storeQuestionsTag(Request $request)
     {
         $this->validate($request,array(
-            'name'        => 'required|string|max:191|unique:tags, name',
+            'name'        => 'required|string|max:191|unique:tags,name',
         ));
 
         $tag = new Tag;
