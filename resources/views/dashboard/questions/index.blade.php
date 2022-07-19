583578e35b0c247e41df5d5c66ec0ec917f231ff
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 02:46:10 +0600

    Commit Wed 07/20/2022  2:46:10.25 1224

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -408,7 +408,7 @@
                       </div>
                       <div class="col-md-6">
                           <div class="input-group mb-3">
-                              <select name="tags_ids[]" class="form-control multiple-select" multiple="multiple" data-placeholder="ট্যাগ" required>
+                              <select name="tags_ids[]" class="form-control multiple-select" multiple="multiple" data-placeholder="ট্যাগ" width="100%" required>
                                   <option></option>
                                   @foreach ($tags as $tag)
                                       <option value="{{ $tag->id }}">{{ $tag->name }}</option>
