5688a967705601f4246a76e557ddbc38bb6e6161
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 01:48:14 +0600

    Commit Wed 07/20/2022  1:48:14.24 16596

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -137,7 +137,7 @@ class APIController extends Controller
     public function getOtherCourseExams($softtoken, $coursetype)
     {
         if($softtoken == 'Rifat.Admin.2022')
-        {
+        {ss
             $course = Course::select('id')
                              ->where('status', 1) // take only active courses
                              ->where('type', $coursetype) // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT
