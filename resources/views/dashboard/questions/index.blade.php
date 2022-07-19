687ce8f11f5c132a8d8a34f4bf2db87330a78c9a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 02:45:32 +0600

    Commit Wed 07/20/2022  2:45:32.28 9994

--- resources/views/dashboard/questions/index.blade.php
+++ resources/views/dashboard/questions/index.blade.php
@@ -517,8 +517,7 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
     $('.multiple-select').select2({
-      placeholder: "Please select an skill",
-      allowClear: true,
+
       theme: 'bootstrap4',
     });
     // ClassicEditor
