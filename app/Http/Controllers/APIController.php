4338d29c3de34462c167e74b2040098832679cf6
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 01:04:02 +0600

    Commit Wed 07/20/2022  1:04:02.24 28302

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -141,8 +141,8 @@ class APIController extends Controller
             $course = Course::select('id')
                              ->where('status', 1) // take only active courses
                              ->where('type', $coursetype) // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT
-                             ->get();
-                             
+                             ->first();
+
             $courseexams = Courseexam::select('course_id', 'exam_id')
                                      ->where('course_id', $id)
                                      ->get();
