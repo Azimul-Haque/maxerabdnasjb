1fe861b9e3a9a29d912eff960c1fe74479018139
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 21:21:23 +0600

    Commit Wed 07/20/2022 21:21:23.34 12964

--- resources/views/index/refundpolicy.blade.php
+++ resources/views/index/refundpolicy.blade.php
@@ -10,7 +10,22 @@
     <div class="container">
 	    <div class="row">
 	      <div class="col-md-12 col-12">
-	      	
+			<p><strong>Purchase Policy:</strong></p>
+			<p>
+			Users will be asked to pay for the respective fees for different Course, Tutorial, Exam. Any price offered for a particular course/tutorial/exam may also be different when you are logged into your account. When you make a purchase, you agree not to use an invalid or unauthorized payment method. We reserve the right to disable access to any course for which we have not received adequate payments. 
+			We may offer free trial for any number of days.
+			At any time and without notice, We reserves the right to (i) modify Terms of Service of Free Trial offer, or (ii) cancel such Free Trial offer.
+			</p> 
+			</p>
+
+			<p><strong>Refund Policy:</strong></p> <p>
+			A Refund Request will be deemed valid only if made through an email to codeminersbd@gmail.com specifying your email address or phone number used during registration within 3 days from the time of purchase. No refund is applicable for courses purchased with less than 12 months validity.
+
+			Refunds shall be made to the bank, mobile financial services account, or card with which the purchase was made within 10 days of the refund request being successfully processed and approved by Codeminers IT Limited.
+			</p> 
+			<p><strong>Contact Us</strong></p> <p>
+			If you have any questions, do not hesitate to contact us at <a href = "mailto: codeminersbd@gmail.com">codeminersbd@gmail.com</a>.
+			</p>
 	      </div>
 	  	</div>
   	</div>
