acc1af513cd344650da6a7f8459c17e2c3c759db
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 18:39:26 +0600

    Commit Wed 07/20/2022 18:39:26.30 20453

---
+++ resources/views/index/refundpolicy.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.index')
+@section('title') BJS & Bar Exam @endsection
+
+@section('third_party_stylesheets')
+
+@endsection
+
+@section('content')
+    
+@endsection
+
+@section('third_party_scripts')
+
+@endsection
