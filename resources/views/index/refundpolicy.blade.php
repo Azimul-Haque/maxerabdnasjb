dfe11822508c63d9f8176fcec2dc00bf1e1001a7
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 21:38:56 +0600

    Commit Wed 07/20/2022 21:38:56.28 1763

--- resources/views/index/refundpolicy.blade.php
+++ resources/views/index/refundpolicy.blade.php
@@ -21,8 +21,9 @@
 			<br/>
 			Within 14 days of the refund request being properly processed and accepted by App Lab IT Authority, refunds will be sent to the bank, mfs account, or card used to make the transaction.
 			</p><br/> 
-			<p><strong>Contact Us</strong></p> <p>
-			If you have any questions, do not hesitate to contact us at <a href = "mailto: codeminersbd@gmail.com">codeminersbd@gmail.com</a>.
+			<p><strong>Contact Us</strong></p>
+			<p>
+			If you have any questions concerning our purchase and refund policy, please contact us at: info@bjsexam.com.
 			</p>
 	      </div>
 	  	</div>
