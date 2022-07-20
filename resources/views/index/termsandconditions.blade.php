94e5c2064924d3819374985598b961b7f48bcdf0
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 18:44:34 +0600

    Commit Wed 07/20/2022 18:44:34.30 14794

--- resources/views/index/termsandconditions.blade.php
+++ resources/views/index/termsandconditions.blade.php
@@ -10,25 +10,7 @@
 <section id="hero-area" class="header-eight">
   <div class="container">
     <div class="row">
-      <div class="col-lg-6 col-md-12 col-12">
-        <div class="header-content">
-          <h1>Best online platform to gear up your BJS & Bar Council Exam preparation!</h1>
-          <p>
-            BJS & BAR EXAM is a dedicated online platform to take the best preparation for the Bangladesh Judicial Service (BJS) Exam and Bar Council Exam.
-          </p>
-          <div class="button">
-            <a href="javascript:void(0)" class="btn primary-btn">Get Started</a>
-            <a href="https://www.youtube.com/watch?v=r44RKWyfcFw&fbclid=IwAR21beSJORalzmzokxDRcGfkZA1AtRTE__l5N4r09HcGS5Y6vOluyouM9EM"
-              class="glightbox video-button">
-              <span class="btn icon-btn rounded-full">
-                <i class="lni lni-play"></i>
-              </span>
-              <span class="text">Watch Intro</span>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-6 col-md-12 col-12">
+      <div class="ccol-md-12 col-12">
         <div class="header-image">
           <img src="{{ asset('/') }}images/header/home.png" alt="#" />
         </div>
