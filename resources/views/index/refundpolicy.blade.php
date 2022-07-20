f8235e5d02d97263c899b8b04f7f0f5ad74fa6f4
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 21:36:32 +0600

    Commit Wed 07/20/2022 21:36:32.34 31051

--- resources/views/index/refundpolicy.blade.php
+++ resources/views/index/refundpolicy.blade.php
@@ -14,9 +14,8 @@
 			<p><strong>Purchase Policy</strong></p>
 			<p>
 			Users will be required to pay the appropriate fees for the various Courses. When you are signed into your account, any pricing that is provided for a specific course can change as well. You consent not to use an illegitimate or unauthorized payment method when you make a transaction. Access to any course may be disabled at our discretion. For any number of days, we may give a free trial. We retain the right to either (a) revise the Free Trial offer's Terms of Service or (b) terminate it at any time and without prior notice.
-			</p> 
 			</p>
-
+			<br/>
 			<p><strong>Refund Policy:</strong></p> <p>
 			A Refund Request will be considered acceptable only if filed within 5 days after purchase by email to info@bjsexam.com or via our App's contact page, mentioning your particular credential used during registration. Courses purchased with less than 6 months validity are not refundable.
 			<br/>
