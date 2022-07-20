db0e30cb9af16ea0b07f1713e1ae0235e66c5bac
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 19:15:08 +0600

    Commit Wed 07/20/2022 19:15:08.45 18920

--- resources/views/index/privacypolicy.blade.php
+++ resources/views/index/privacypolicy.blade.php
@@ -40,7 +40,12 @@
 			</p><br/> <p>
 			Link to the privacy policy of third-party service providers used
 			by the app
-			</p> <ul><li><a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a></li><!----><li><a href="https://firebase.google.com/policies/analytics" target="_blank" rel="noopener noreferrer">Google Analytics for Firebase</a></li><li><a href="https://firebase.google.com/support/privacy/" target="_blank" rel="noopener noreferrer">Firebase Crashlytics</a></li><li><a href="https://www.facebook.com/about/privacy/update/printable" target="_blank" rel="noopener noreferrer">Facebook</a></li><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----></ul></div>
+			</p>
+			<ul>
+				<li>■ <a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a></li><!---->
+				<li>■ <a href="https://firebase.google.com/policies/analytics" target="_blank" rel="noopener noreferrer">Google Analytics for Firebase</a></li>
+				<li>■ <a href="https://firebase.google.com/support/privacy/" target="_blank" rel="noopener noreferrer">Firebase Crashlytics</a></li>
+				<li>■ <a href="https://www.facebook.com/about/privacy/update/printable" target="_blank" rel="noopener noreferrer">Facebook</a></li><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----></ul></div>
 			<br/>
 			<p><strong>Log Data</strong></p> <p>
 			We want to inform you that whenever you
