a1a8c850059986b4cd002a4ba9947b206648ee43
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 21:36:16 +0600

    Commit Wed 07/20/2022 21:36:16.24 24847

--- resources/views/index/refundpolicy.blade.php
+++ resources/views/index/refundpolicy.blade.php
@@ -18,7 +18,9 @@
 			</p>
 
 			<p><strong>Refund Policy:</strong></p> <p>
-			A Refund Request will be considered acceptable only if filed within 5 days after purchase by email to info@bjsexam.com or via our App's contact page, mentioning your particular credential used during registration. Courses purchased with less than 6 months validity are not refundable. Within 14 days of the refund request being properly processed and accepted by App Lab IT Authority, refunds will be sent to the bank, mfs account, or card used to make the transaction.
+			A Refund Request will be considered acceptable only if filed within 5 days after purchase by email to info@bjsexam.com or via our App's contact page, mentioning your particular credential used during registration. Courses purchased with less than 6 months validity are not refundable.
+			<br/>
+			Within 14 days of the refund request being properly processed and accepted by App Lab IT Authority, refunds will be sent to the bank, mfs account, or card used to make the transaction.
 			</p> 
 			<p><strong>Contact Us</strong></p> <p>
 			If you have any questions, do not hesitate to contact us at <a href = "mailto: codeminersbd@gmail.com">codeminersbd@gmail.com</a>.
