9b7331e39e8109863f75ea3ae1b6c7c9c1bae2ed
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-20 18:49:22 +0600

    Commit Wed 07/20/2022 18:49:22.29 10263

--- resources/views/index/termsandconditions.blade.php
+++ resources/views/index/termsandconditions.blade.php
@@ -7,7 +7,7 @@
 
 @section('content')
 
-<section style="padding-top: 150px; padding-bottom: 30px; background-color: var(--light-3);">
+<section style="padding-top: 150px; padding-bottom: 50px; background-color: var(--light-3);">
   <div class="container">
     <div class="row">
       <div class="ccol-md-12 col-12">
