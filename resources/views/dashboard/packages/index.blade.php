b553579320980667f81916861c1b1632faf01aa2
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-22 17:05:26 +0600

    Commit Fri 07/22/2022 17:05:26.24 11047

--- resources/views/dashboard/packages/index.blade.php
+++ resources/views/dashboard/packages/index.blade.php
@@ -185,7 +185,7 @@
 	                           name="name"
 	                           class="form-control"
 	                           value="{{ old('name') }}"
-	                           placeholder="নাম" required>
+	                           placeholder="প্যাকেজের নাম" required>
 	                    <div class="input-group-append">
 	                        <div class="input-group-text"><span class="fas fa-ticket-alt"></span></div>
 	                    </div>
