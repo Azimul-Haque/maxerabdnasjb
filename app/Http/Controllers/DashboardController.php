7c6a513d64d16a6d2f505adfc4130c7a6c56e495
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-22 16:49:16 +0600

    Commit Fri 07/22/2022 16:49:16.23 230

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -239,19 +239,6 @@ class DashboardController extends Controller
     public function getPackages()
     {
         
-        $users = User::whereNotIn('mobile', ['01751398392', '01837409842'])->get();
-        $totalbalance = Balance::sum('amount');
-        $totalexpense = Expense::sum('amount');
-
-        $balances = Balance::where('amount', '>', 0)
-                           ->orderBy('id', 'desc')
-                           ->paginate(5);
-
-        return view('balances.index')
-                    ->withUsers($users)
-                    ->withBalances($balances)
-                    ->withTotalbalance($totalbalance)
-                    ->withTotalexpense($totalexpense);
     }
 
     public function getBalance()
