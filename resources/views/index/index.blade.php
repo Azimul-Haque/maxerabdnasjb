6368c5d7a59af8e29feb9be2dd36fdb14561c455
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-22 16:40:06 +0600

    Commit Fri 07/22/2022 16:40:06.25 2492

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -334,7 +334,7 @@
                   <p>৩০ দিনের জন্য অ্যাপের সব ফিচার অ্যাভেইলেবল থাকবে</p>
                   <div class="price">
                     <h2 class="amount">
-                      <span class="currency"><strike>৳ ৫০</strike></span> ৳ ৩৯
+                      <span class="currency"><strike>৳ ৫০</strike></span> ৳ ৩৯<span class="duration">/৩০ দিন </span>
                     </h2>
                   </div>
               </div>
