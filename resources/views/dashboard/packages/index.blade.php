b4fcf52513bce62ab970be525e04a1245809bf91
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-24 23:51:06 +0600

    Commit Sun 07/24/2022 23:51:06.31 22389

--- resources/views/dashboard/packages/index.blade.php
+++ resources/views/dashboard/packages/index.blade.php
@@ -323,7 +323,7 @@
 
     <script type="text/javascript">
       // Import the functions you need from the SDKs you need
-      import { initializeApp };
+      import { initializeApp } from 'firebase/app';
       // TODO: Add SDKs for Firebase products that you want to use
       // https://firebase.google.com/docs/web/setup#available-libraries
 
