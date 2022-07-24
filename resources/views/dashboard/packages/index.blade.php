ef99bad7093fd944b350e78e31de30da421c6b7c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-24 23:58:05 +0600

    Commit Sun 07/24/2022 23:58:05.24 19543

--- resources/views/dashboard/packages/index.blade.php
+++ resources/views/dashboard/packages/index.blade.php
@@ -323,7 +323,7 @@
       import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js';
 
       // import { auth } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js';
-      import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-firestore.js';
+      import { collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-firestore.js';
 
       const firebaseConfig = {
         apiKey: "AIzaSyA2Xh9xV12QMH2gD6-8Oy89ofagRBxffVo",
@@ -336,12 +336,10 @@
       };
 
       const app = initializeApp(firebaseConfig);
-      const db = getFirestore(app);
-
-      db.collection("packages").get().then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-              console.log(`${doc.id} => ${doc.data()}`);
-          });
+      
+      const querySnapshot = await getDocs(collection(db, "users"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
       });
     </script>
 @endsection
