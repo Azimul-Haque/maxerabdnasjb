81571459383af1470f884cf0050a7bc605a5412b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 01:24:32 +0600

    Commit Mon 07/25/2022  1:24:32.30 856

--- resources/views/dashboard/packages/index.blade.php
+++ resources/views/dashboard/packages/index.blade.php
@@ -362,6 +362,7 @@
       });
       console.log(packages);
 
+      const sfDocRef = doc(db, "packages", "SF");
       try {
         await runTransaction(db, async (transaction) => {
           const sfDoc = await transaction.get(sfDocRef);
