3eaf1c28ec83cc66fa689009848ae9122272539a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-24 23:23:47 +0600

    Commit Sun 07/24/2022 23:23:47.25 25493

--- resources/views/dashboard/packages/index.blade.php
+++ resources/views/dashboard/packages/index.blade.php
@@ -13,7 +13,7 @@
             <h3 class="card-title">প্যাকেজ তালিকা</h3>
 
             <div class="card-tools">
-            	<button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addPackageModal" rel="tooltip" data-original-title="প্যাকেজ যোগ করুন">
+            	<button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addPackageModal" title="" rel="tooltip" data-original-title="প্যাকেজ যোগ করুন">
             		<i class="fas fa-clipboard-check"></i> নতুন প্যাকেজ
             	</button>
             </div>
