c176b61577c70294a38c244e1bd95dd24c44a333
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-25 00:40:55 +0600

    Commit Mon 07/25/2022  0:40:55.27 21770

--- resources/views/dashboard/packages/index.blade.php
+++ resources/views/dashboard/packages/index.blade.php
@@ -320,46 +320,46 @@
     </script>
 
     <script type="module">
-      // import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js';
+      import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js';
 
-      // // import { auth } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js';
-      // import { getFirestore, collection, getDocs, addDoc } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-firestore.js';
+      // import { auth } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js';
+      import { getFirestore, collection, getDocs, addDoc } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-firestore.js';
 
-      // const firebaseConfig = {
-      //   apiKey: "AIzaSyA2Xh9xV12QMH2gD6-8Oy89ofagRBxffVo",
-      //   authDomain: "bjsexam-fdcc8.firebaseapp.com",
-      //   projectId: "bjsexam-fdcc8",
-      //   storageBucket: "bjsexam-fdcc8.appspot.com",
-      //   messagingSenderId: "708255220657",
-      //   appId: "1:708255220657:web:4f60732f0148ed95c73390",
-      //   measurementId: "G-X53FCGGDKM"
-      // };
+      const firebaseConfig = {
+        apiKey: "AIzaSyA2Xh9xV12QMH2gD6-8Oy89ofagRBxffVo",
+        authDomain: "bjsexam-fdcc8.firebaseapp.com",
+        projectId: "bjsexam-fdcc8",
+        storageBucket: "bjsexam-fdcc8.appspot.com",
+        messagingSenderId: "708255220657",
+        appId: "1:708255220657:web:4f60732f0148ed95c73390",
+        measurementId: "G-X53FCGGDKM"
+      };
 
-      // const app = initializeApp(firebaseConfig);
-      // const db = getFirestore(app);
+      const app = initializeApp(firebaseConfig);
+      const db = getFirestore(app);
 
-      // try {
-      //   const docRef = await addDoc(collection(db, "packages"), {
-      //     name: "Test 333",
-      //     tagline: "Test Tagline 2333",
-      //     duration: "৯০ দিন",
-      //     price: "99",
-      //     strike_price: "150",
-      //     status: 1,
-      //     suggested: 0
-      //   });
+      try {
+        const docRef = await addDoc(collection(db, "packages"), {
+          name: "Test 333",
+          tagline: "Test Tagline 2333",
+          duration: "৯০ দিন",
+          price: "99",
+          strike_price: "150",
+          status: 1,
+          suggested: 0
+        });
 
-      //   console.log("Document written with ID: ", docRef.id);
-      // } catch (e) {
-      //   console.error("Error adding document: ", e);
-      // }
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
 
-      // const querySnapshot = await getDocs(collection(db, "packages"));
-      // var packages = [];
-      // querySnapshot.forEach((doc) => {
-      //   console.log(`${doc.id} => ${doc.data()}`);
-      //   packages.push(doc.data());
-      // });
-      // console.log(packages);
+      const querySnapshot = await getDocs(collection(db, "packages"));
+      var packages = [];
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        packages.push(doc.data());
+      });
+      console.log(packages);
     </script>
 @endsection
