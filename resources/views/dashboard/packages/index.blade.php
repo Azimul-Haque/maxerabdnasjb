4a361eaf6a5d91f93c202d5fd9edb8898d446454
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-24 23:52:54 +0600

    Commit Sun 07/24/2022 23:52:54.27 8187

--- resources/views/dashboard/packages/index.blade.php
+++ resources/views/dashboard/packages/index.blade.php
@@ -328,9 +328,12 @@
       // Add Firebase products that you want to use
       import { auth } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js'
       import { firestore } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-firestore.js'
-    </script>
+      
+      // Import the functions you need from the SDKs you need
+      import { initializeApp } from 'firebase/app';
+      // TODO: Add SDKs for Firebase products that you want to use
+      // https://firebase.google.com/docs/web/setup#available-libraries
 
-    <script type="text/javascript">
       // Your web app's Firebase configuration
       // For Firebase JS SDK v7.20.0 and later, measurementId is optional
       const firebaseConfig = {
