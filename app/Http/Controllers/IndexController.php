bfae4612b05adc137744131119d33935adedb3f4
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-28 01:12:15 +0600

    Commit Thu 07/28/2022  1:12:15.23 6214

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -77,7 +77,8 @@ class IndexController extends Controller
         if($user) {
             $temppayment = new Temppayment;
             $temppayment->user_id = $user->id;
-            $temppayment->user_id = $user->id;
+            $temppayment->package_id = $request->package_id;
+            $temppayment->package_id = $request->package_id;
         } else {
             Session::flash('warning','নাম্বারটি পাওয়া যায়নি! আগে রেজিস্ট্রেশন করুন।');
             return redirect()->route('index.index');
