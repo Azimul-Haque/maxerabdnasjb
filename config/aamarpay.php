6a978ea274fc3ec213e0706898627fb17cd9d4a4
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 16:30:17 +0600

    Commit Wed 07/27/2022 16:30:17.31 17462

--- config/aamarpay.php
+++ config/aamarpay.php
@@ -3,7 +3,7 @@
 return [
     'store_id' => env('AAMARPAY_STORE_ID','bjsexam'),
     'signature_key' => env('AAMARPAY_KEY','24b07a0a021706e13b79880bcf7c4033'),
-    'sandbox' => env('AAMARPAY_SANDBOX', true),
+    'sandbox' => env('AAMARPAY_SANDBOX', false),
     'redirect_url' => [
         'success' => [
             'url' => env('AAMARPAY_SUCCESS_URL','') // payment.success
