7539160ff4606413ffbf1b079a64ff51ee08b239
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 17:13:12 +0600

    Commit Wed 07/27/2022 17:13:12.31 31603

--- database/migrations/2022_07_27_171059_create_payments_table.php
+++ database/migrations/2022_07_27_171059_create_payments_table.php
@@ -16,6 +16,7 @@ class CreatePaymentsTable extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
+            $table->string('uid');
             $table->integer('payer_id')->unsigned();
             $table->text('bulk_payment_member_ids');
             $table->integer('payment_status')->unsigned();
