ab69f62bcdba5cab1f972a736646c945c29e15fc
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 19:00:29 +0600

    Commit Wed 07/27/2022 19:00:29.25 10865

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -374,7 +374,7 @@
                   <div class="modal-body">
                     <b>প্যাকেজঃ</b> {{ $package->name }} ({{ $package->tagline }})<br/>
                     <b>প্যাকেজের মেয়াদঃ</b> {{ $package->duration }}<br/>
-                    <big><b>প্যাকেজের মূল্যঃ</b> ৳ {{ bangla($package->price) }}<br/></big>
+                    <big><b>প্যাকেজের মূল্যঃ</b> <strike>৳ {{ bangla($package->strike_price) }}<strike> ৳ {{ bangla($package->price) }}<br/></big>
                   </div>
 
                   <!-- Modal footer -->
