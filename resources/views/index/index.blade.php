d446ed45ecc3b13e444107808c0cd9a1116312ee
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 19:14:06 +0600

    Commit Wed 07/27/2022 19:14:06.29 14089

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -392,7 +392,7 @@
                     {{-- <button type="button" class="btn btn-danger" data-bs-dismiss="modal">ফিরে যান</button> --}}
                     <form method="post" id="" action="{{ route('index.payment.proceed') }}">
                       @csrf
-                      <input type="hidden" name="package_id">
+                      <input type="hidden" name="package_id" value="{{ $package->id }}">
                       <button type="submit" class="btn primary-btn-outline" data-bs-dismiss="modal">৳ {{ bangla($package->price) }} পরিশোধ করুন</button>
                     </form>
                   </div>
