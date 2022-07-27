ffd49306c18b85fb8f00746108fe97f8bd21293c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 17:12:45 +0600

    Commit Wed 07/27/2022 17:12:45.26 31804

--- database/migrations/2022_07_27_171059_create_payments_table.php
+++ database/migrations/2022_07_27_171059_create_payments_table.php
@@ -14,7 +14,22 @@ class CreatePaymentsTable extends Migration
     public function up()
     {
         Schema::create('payments', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('payer_id')->unsigned();
+            $table->text('bulk_payment_member_ids');
+            $table->integer('payment_status')->unsigned();
+            $table->integer('payment_category')->unsigned();
+            $table->string('payment_type');
+            $table->string('payment_key');
+            $table->integer('payment_method')->nullable();
+            $table->string('card_type')->nullable();
+            $table->string('amount');
+            $table->string('bank');
+            $table->string('branch');
+            $table->string('pay_slip');
+            $table->integer('is_archieved')->unsigned();
+            $table->foreign('member_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
