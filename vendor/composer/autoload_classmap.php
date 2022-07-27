1c87eea27eb1ac6684b58de0a76df69b2006de78
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 17:11:08 +0600

    Commit Wed 07/27/2022 17:11:08.34 198

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -43,6 +43,7 @@ return array(
     'App\\Http\\Middleware\\TrustProxies' => $baseDir . '/app/Http/Middleware/TrustProxies.php',
     'App\\Http\\Middleware\\VerifyCsrfToken' => $baseDir . '/app/Http/Middleware/VerifyCsrfToken.php',
     'App\\Package' => $baseDir . '/app/Package.php',
+    'App\\Payment' => $baseDir . '/app/Payment.php',
     'App\\Providers\\AppServiceProvider' => $baseDir . '/app/Providers/AppServiceProvider.php',
     'App\\Providers\\AuthServiceProvider' => $baseDir . '/app/Providers/AuthServiceProvider.php',
     'App\\Providers\\BroadcastServiceProvider' => $baseDir . '/app/Providers/BroadcastServiceProvider.php',
