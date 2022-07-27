cfcc2e070ea820fa25263b703d3261bfedc1e890
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-28 01:10:53 +0600

    Commit Thu 07/28/2022  1:10:53.25 16699

--- resources/views/partials/_messages.blade.php
+++ resources/views/partials/_messages.blade.php
@@ -5,7 +5,7 @@
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
-    timer: 3000,
+    timer: 3500,
     timerProgressBar: true,
     didOpen: (toast) => {
       toast.addEventListener('mouseenter', Swal.stopTimer)
