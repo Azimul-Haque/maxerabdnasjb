dbca5304e551d1a070acbd4830c164393c0395d9
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-27 17:17:41 +0600

    Commit Wed 07/27/2022 17:17:41.28 6398

--- database/migrations/2022_07_27_171059_create_payments_table.php
+++ database/migrations/2022_07_27_171059_create_payments_table.php
@@ -22,7 +22,7 @@ class CreatePaymentsTable extends Migration
             $table->string('card_type')->nullable();
             $table->string('trx_id');
             $table->string('amount');
-            $table->foreign('member_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
