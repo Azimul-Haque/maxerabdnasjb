0dc7de84649263e70d1e953d64c251f3f3777e0e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-28 01:02:46 +0600

    Commit Thu 07/28/2022  1:02:46.31 11146

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -400,6 +400,7 @@
                         {{-- <button type="button" class="btn btn-danger" data-bs-dismiss="modal">ফিরে যান</button> --}}
                         <input type="hidden" name="package_id" value="{{ $package->id }}" required>
                         <button type="submit" class="btn primary-btn-outline">৳ {{ bangla($package->price) }} পরিশোধ করুন</button>
+                        <button type="submit" class="btn btn-danger" >ফিরে যান</button>
                     </div>
                   </form>
                 </div>
