0cfa49c57c3d0667ad4007b2ecb9b8e85f5d3560
Azimul-Haque <orbachinujbuk@gmail.com>
2022-07-29 23:10:37 +0600

    Commit Fri 07/29/2022 23:10:37.30 4180

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -119,7 +119,7 @@ class IndexController extends Controller
         // }
         $valid  = Aamarpay::valid($request, $amount_request);
         if($valid) {
-            Session::flash('info', 'পেমেন্ট সম্পন্ন হয়নি, আবার চেষ্টা করুন!');
+            Session::flash('success', 'পেমেন্ট সফল হয়েছে। ধন্যবাদ!');
             return redirect()->route('index.index');
         } else {
             Session::flash('info', 'পেমেন্ট সম্পন্ন হয়নি, অনুগ্রহ করে Contact ফর্ম এর মাধ্যমে আমাদের জানান।');
