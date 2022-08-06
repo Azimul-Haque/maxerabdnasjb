41a7e9b82885bf099cf20dc7eb99853658fdcba3
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 02:00:41 +0600

    Commit Sun 08/07/2022  2:00:41.27 15493

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -133,6 +133,7 @@ class IndexController extends Controller
             $user = User::findOrFail($user_id);
             $current_package_date = Carbon::parse($user->package_expiry_date);
             dd($current_package_date->greaterThanOrEqualTo(Carbon::now()));
+            $package = Package::findOrFail($temppayment->package_id);
             if($current_package_date->greaterThanOrEqualTo(Carbon::now())) {
                 $package_expiry_date = $current_package_date->addDays(1)->format('Y-m-d') . ' 23:59:59';
             }
