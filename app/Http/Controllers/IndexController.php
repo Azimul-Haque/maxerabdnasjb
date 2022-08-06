3f59250001896df28466b204f109fe44d6eb30d1
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 01:49:48 +0600

    Commit Sun 08/07/2022  1:49:48.25 17333

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -119,7 +119,7 @@ class IndexController extends Controller
             // OLD VERIFICATION METHOD
             
             $temppayment = Temppayment::where('trx_id', $request->mer_txnid)->first();
-            dd($request->all());
+            // dd($request->all());
             $payment = new Payment;
             $payment->user_id = $user_id;
             $payment->package_id = $temppayment->package_id;
