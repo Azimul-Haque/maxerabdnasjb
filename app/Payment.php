f362da735579a170ce2a1b4eaffee829d99b6e7d
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 02:16:46 +0600

    Commit Sun 08/07/2022  2:16:46.32 32129

--- app/Payment.php
+++ app/Payment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    public function payment() {
+    public function user() {
       return $this->belongsTo('App\User');
     }
 }
