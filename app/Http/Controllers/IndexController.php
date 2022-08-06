8289c5deb52701d40fa6804859453ffb3baf96c8
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 01:02:23 +0600

    Commit Sun 08/07/2022  1:02:23.27 1564

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -117,10 +117,9 @@ class IndexController extends Controller
 
         if($amount_paid == $amount_request) {
             // OLD VERIFICATION METHOD
-            echo 'etai kaaj kore';
-            // dd(number_format($amount_request, 2));
+            dd($request->all());
         }
-        $valid  = Aamarpay::valid($request, number_format($amount_request, 2));
+        $valid  = Aamarpay::valid($request, $amount_request);
         dd($valid);
         if($valid)
         {
