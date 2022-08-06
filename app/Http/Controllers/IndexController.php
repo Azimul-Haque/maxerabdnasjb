54442efd2494c6b1ad4f12b2d2ff0e225844f452
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 00:54:43 +0600

    Commit Sun 08/07/2022  0:54:43.23 11795

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -117,7 +117,7 @@ class IndexController extends Controller
 
         if($amount_paid == $amount_request) {
            // OLD VERIFICATION METHOD
-            
+           dd($request->all());
         }
         $valid  = Aamarpay::valid($request, $amount_request);
         
