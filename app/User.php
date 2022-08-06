a9b694613e88000ac5ce776544efac0abe1e434a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 02:16:07 +0600

    Commit Sun 08/07/2022  2:16:07.33 31905

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
     use Notifiable;
 
     public function payments(){
-        return $this->hasMany('App\Balance');
+        return $this->hasMany('App\Payment');
     }
 
     // public function balances(){
