92e5e61049993949191a10b0b822be5eab8d37ec
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 02:05:17 +0600

    Commit Sun 08/07/2022  2:05:17.29 29230

--- resources/views/dashboard/packages/index.blade.php
+++ resources/views/dashboard/packages/index.blade.php
@@ -264,7 +264,7 @@
                       <input type="number"
                              name="numeric_duration"
                              class="form-control"
-                             value="{{ $package->numeric_duration }}"
+                             value="{{ old('numeric_duration') }}"
                              placeholder="নম্বরে মেয়াদ" required>
                       <div class="input-group-append">
                           <div class="input-group-text"><span class="fas fa-calendar-check"></span></div>
