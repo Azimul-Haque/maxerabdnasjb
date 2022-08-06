dd542e90985ae9fc87809a95f110069be9dcb77e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 01:45:55 +0600

    Commit Sun 08/07/2022  1:45:55.29 5040

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -132,7 +132,6 @@ class IndexController extends Controller
 
             $user = User::findOrFail($user_id);
             $package_expiry_date = Carbon::now()->addDays(1)->format('Y-m-d') . ' 23:59:59';
-            // dd($package_expiry_date);
             $user->package_expiry_date = $package_expiry_date;
 
             $payment->save();
