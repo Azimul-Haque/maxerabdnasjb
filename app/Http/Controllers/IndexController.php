2bde11a0b1b6f5cfc9b67aa4505593ef4fcc5cf2
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 01:08:53 +0600

    Commit Sun 08/07/2022  1:08:53.27 6815

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -124,6 +124,7 @@ class IndexController extends Controller
             $payment->user_id = $user_id;
             $payment->package_id = $temppayment->package_id;
             $payment->uid = $temppayment->uid;
+            $payment->payment_status = $temppayment->payment_status;
             Session::flash('success', 'পেমেন্ট সফল হয়েছে। ধন্যবাদ!');
             return redirect()->route('index.index');
         } else {
