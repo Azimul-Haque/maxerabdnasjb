01a116db45a2229873646b03099633a90011f43b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-07 02:17:48 +0600

    Commit Sun 08/07/2022  2:17:48.23 19877

--- app/Payment.php
+++ app/Payment.php
@@ -9,4 +9,8 @@ class Payment extends Model
     public function user() {
       return $this->belongsTo('App\User');
     }
+
+    public function user() {
+      return $this->belongsTo('App\User');
+    }
 }
