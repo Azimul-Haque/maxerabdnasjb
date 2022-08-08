045410c370ca549387e0c7da841ea6afe5e79600
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-09 04:42:07 +0600

    Commit Tue 08/09/2022  4:42:07.33 29104

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -154,8 +154,8 @@ class IndexController extends Controller
             return redirect()->route('index.index');
         } else {
             // dd($request->all());
-            $paymentdata = json_encode($request->all());
-            Session::flash('swalsuccess', $paymentdata);
+            // $paymentdata = json_encode($request->all());
+            // Session::flash('swalsuccess', $paymentdata);
             Session::flash('info', 'পেমেন্ট সম্পন্ন হয়নি, অনুগ্রহ করে Contact ফর্ম এর মাধ্যমে আমাদের জানান।');
             return redirect()->route('index.index');
         }
