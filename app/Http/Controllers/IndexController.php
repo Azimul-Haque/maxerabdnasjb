b80d1742be7c28f4f5de9bb3f566420d1eb8858c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-09 04:28:26 +0600

    Commit Tue 08/09/2022  4:28:26.30 26593

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -155,7 +155,7 @@ class IndexController extends Controller
         } else {
             // dd($request->all());
             $paymentdata = json_encode($request->all());
-            return view('index.payments.errors')->withPayment($paymentdata);
+            return view('index.payments.errors')->withPaymentdata($paymentdata);
             Session::flash('info', 'পেমেন্ট সম্পন্ন হয়নি, অনুগ্রহ করে Contact ফর্ম এর মাধ্যমে আমাদের জানান।');
             return redirect()->route('index.index');
         }
--- resources/views/index/payments/errors.blade.php
+++ resources/views/index/payments/errors.blade.php
@@ -1,3 +1,3 @@
 <?php
 
-print_r($encodedSku);
+print_r($paymentdata);
