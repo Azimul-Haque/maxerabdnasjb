423c487c3ca8215bd8a2ca6d424f95baad35134b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-13 13:51:08 +0600

    Commit Sat 08/13/2022 13:51:08.32 11707

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/testapi', 'APIController@test')->name('api.test');
 
 Route::get('/checkuid/{softtoken}/{phonenumber}', 'APIController@checkUid')->name('api.checkuid');
-Route::get('/checkpackagevalidity/{softtoken}/{uid}', 'APIController@checkPackageValidity')->name('api.checkpackagevalidity');
+Route::get('/checkpackagevalidity/{softtoken}/{phonenumber}', 'APIController@checkPackageValidity')->name('api.checkpackagevalidity');
 Route::post('/adduser', 'APIController@addUser')->name('api.adduser');
 Route::post('/updateuser', 'APIController@updateUser')->name('api.updateuser');
 
