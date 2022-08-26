729df1c3b1444ee4407d304ff4a5cf659bd1cc14
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-27 05:25:08 +0600

    Commit Sat 08/27/2022  5:25:08.24 22930

--- resources/views/dashboard/exams/index.blade.php
+++ resources/views/dashboard/exams/index.blade.php
@@ -485,7 +485,7 @@
                         </div>
                         <div class="col-md-12">
                             <div class="input-group mb-3">
-                                <textarea class="form-control" style="height: 150px;" placeholder="সিলেবাস লিখুন (যদি থাকে)"></textarea>
+                                <textarea class="form-control" name="syllabus" style="height: 150px;" placeholder="সিলেবাস লিখুন (যদি থাকে)"></textarea>
                             </div>
                         </div>
                     </div>
