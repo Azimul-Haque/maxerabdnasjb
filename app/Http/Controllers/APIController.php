4bac9b0759701e189426de41280f60c790d4a071
Azimul-Haque <orbachinujbuk@gmail.com>
2022-08-27 05:34:54 +0600

    Commit Sat 08/27/2022  5:34:54.23 11864

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -148,7 +148,7 @@ class APIController extends Controller
             foreach($courseexams as $courseexam) {
                 $courseexam->name = $courseexam->exam->name;
                 $courseexam->start = $courseexam->exam->available_from;
-                $courseexam->syllabus = $courseexam->exam->syllabus;
+                $courseexam->syllabus = $courseexam->exam->syllabus ? $courseexam->exam->syllabus : 'N/A';
             }
             // $courseexams = $courseexams->sortByDesc('start');
             return response()->json([
