f26927273d89fc33e81d2bed6b28ced051fe6213
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-05 02:30:45 +0600

    Commit Mon 09/05/2022  2:30:45.23 26196

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -205,7 +205,7 @@ class APIController extends Controller
             foreach($examquestions as $examquestion) {
                 $examquestion = $examquestion->makeHidden(['question_id']);
                 $examquestion->question->explanation = $examquestion->question->questionexplanation ? $examquestion->question->questionexplanation->explanation : '';
-                $examquestion->question = $examquestion->question->makeHidden(['topic_id', 'difficulty', 'created_at', 'updated_at']);
+                $examquestion->question = $examquestion->question->makeHidden(['topic_id', 'difficulty', 'created_at', 'updated_at', 'questionexplanation']);
             }
 
             return response()->json([
