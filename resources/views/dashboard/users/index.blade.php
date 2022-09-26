223cd640a0e21339ab08315786cf4e6e482c879d
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-27 02:49:04 +0600

    Commit Tue 09/27/2022  2:49:04.32 15859

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -3,6 +3,7 @@
 
 @section('third_party_stylesheets')
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap-datepicker.min.css') }}">
 @endsection
 
 @section('content')
