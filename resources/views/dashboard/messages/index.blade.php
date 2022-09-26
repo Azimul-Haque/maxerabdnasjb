c834900e91c7b4eb0ea147d9984bf31fca688b5d
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-27 01:26:31 +0600

    Commit Tue 09/27/2022  1:26:31.32 6511

--- resources/views/dashboard/messages/index.blade.php
+++ resources/views/dashboard/messages/index.blade.php
@@ -22,16 +22,6 @@
           <div class="card-body p-0">
             <table class="table">
               <tbody>
-                {{-- <tr>
-                  <td>1.</td>
-                  <td>Update software</td>
-                  <td>
-                    <div class="progress progress-xs">
-                      <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                    </div>
-                  </td>
-                  <td><span class="badge bg-danger">55%</span></td>
-                </tr> --}}
                 @foreach($packages as $package)
                 	<tr>
                 		<td>
