717e6d07a6111c3c22da783c0e8e479d043f55da
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-27 01:28:36 +0600

    Commit Tue 09/27/2022  1:28:36.22 19795

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -62,7 +62,7 @@
 
 @if(Auth::user()->role == 'admin' || Auth::user()->role == 'manager')
 <li class="nav-item">
-    <a href="{{ route('dashboard.courses') }}" class="nav-link {{ Request::is('dashboard/courses') ? 'active' : '' }} {{ Request::is('dashboard/courses/*') ? 'active' : '' }}">
+    <a href="{{ route('dashboard.messages') }}" class="nav-link {{ Request::is('dashboard/courses') ? 'active' : '' }} {{ Request::is('dashboard/courses/*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-layer-group"></i>
         <p>কোর্সসমূহ</p>
     </a>
