874e5f7c12b82b6fc7ea73a9798abb23edefba87
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-27 00:54:45 +0600

    Commit Tue 09/27/2022  0:54:45.11 31778

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('role')->default('user');
             $table->string('mobile')->unique();
+            $table->string('bkash');
             $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
