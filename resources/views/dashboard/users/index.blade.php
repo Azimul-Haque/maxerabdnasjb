d3f779e8ff0263e873d49b18fc5c5f983c0ad30c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-27 02:45:52 +0600

    Commit Tue 09/27/2022  2:45:52.29 27929

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -266,7 +266,7 @@
         });
     </script>
 
-    <script type="text/javascript" src="{{ asset('js/jquery-for-dp.min.js') }}"></script>
+    {{-- <script type="text/javascript" src="{{ asset('js/jquery-for-dp.min.js') }}"></script> --}}
     <script type="text/javascript" src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <script>
