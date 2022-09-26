2709d6c439aaadba9876045e3513a38022debb8c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-27 01:33:58 +0600

    Commit Tue 09/27/2022  1:33:58.26 6019

--- resources/views/dashboard/messages/index.blade.php
+++ resources/views/dashboard/messages/index.blade.php
@@ -50,7 +50,7 @@
                           <div class="modal-dialog" role="document">
                             <div class="modal-content">
                               <div class="modal-header bg-danger">
-                                <h5 class="modal-title" id="deleteModalLabel">প্যাকেজ ডিলেট</h5>
+                                <h5 class="modal-title" id="deleteModalLabel">মেসেজ ডিলেট</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                   <span aria-hidden="true">&times;</span>
                                 </button>
