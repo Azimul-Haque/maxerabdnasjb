cb77b8b0a04ce60ea6995c6bca80904ab4a83b32
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-27 01:30:47 +0600

    Commit Tue 09/27/2022  1:30:47.26 5165

--- resources/views/dashboard/messages/index.blade.php
+++ resources/views/dashboard/messages/index.blade.php
@@ -34,7 +34,7 @@
                 	<tr>
                     <td>{{ $message->user->name }}</td>
                     <td>{{ $message->user->mobile }}</td>
-                    <td>{{ $message->user->payments->count() }} বার</td>
+                    <td>{{ $message->user->payments->count() }} বার কিনেছেন</td>
                 		<td>
                 			{{ $message->name }}
                       <span class="badge bg-info"><strike>৳ {{ $message->strike_price }}</strike></span>
