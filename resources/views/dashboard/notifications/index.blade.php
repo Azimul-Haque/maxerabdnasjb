41977b37fd6a58e0b7ed33c0b0e82539e18bde18
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 01:31:22 +0600

    Commit Wed 09/28/2022  1:31:22.31 21308

--- resources/views/dashboard/notifications/index.blade.php
+++ resources/views/dashboard/notifications/index.blade.php
@@ -148,7 +148,7 @@
 @section('third_party_scripts')
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript">
-        $('#headings').change(function () {
+        $('#headings').keydow(function () {
           $('#headingstext').text(this.value);
         });
     </script>
