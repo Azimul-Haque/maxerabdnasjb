6b0649bf1a33114045e4f19159180bbd2858e7cd
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 01:46:41 +0600

    Commit Wed 09/28/2022  1:46:41.23 24662

--- resources/views/dashboard/notifications/index.blade.php
+++ resources/views/dashboard/notifications/index.blade.php
@@ -48,7 +48,7 @@
                                 <div class="modal-dialog" role="document">
                                   <div class="modal-content">
                                     <div class="modal-header bg-danger">
-                                      <h5 class="modal-title" id="deleteModalLabel">মেসেজ ডিলেট</h5>
+                                      <h5 class="modal-title" id="deleteModalLabel">নোটিফিকেশন ডিলেট</h5>
                                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                       </button>
