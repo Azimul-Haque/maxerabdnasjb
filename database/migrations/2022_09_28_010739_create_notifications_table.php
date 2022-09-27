92263a81c67e08ae6e126d48575344bebebee9c3
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 01:07:58 +0600

    Commit Wed 09/28/2022  1:07:58.29 8890

--- database/migrations/2022_09_28_010739_create_notifications_table.php
+++ database/migrations/2022_09_28_010739_create_notifications_table.php
@@ -15,6 +15,8 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->string('message');
             $table->timestamps();
         });
     }
