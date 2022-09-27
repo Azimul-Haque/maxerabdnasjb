afb53edece9f42d5d9d3de83b18c28bcb43d20db
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 02:16:50 +0600

    Commit Wed 09/28/2022  2:16:50.35 2102

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -894,7 +894,12 @@ class DashboardController extends Controller
         } elseif($request->type == 'premium') {
             OneSignal::sendNotificationUsingTags(
                 "Some Message",$request->message,
-                array("field" => "tag", "key" => "user_type", "relation" => "=", "value" => "Premium"),
+                array(
+                    "field" => "tag",
+                    "key" => "user_type",
+                    "relation" => "=",
+                    "value" => "Premium"
+                ),
                 $url = null,
                 $data = null,
                 $buttons = null,
