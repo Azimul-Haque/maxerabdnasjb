73c055f57742362031246546735d83c0a7adcc55
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 02:54:39 +0600

    Commit Wed 09/28/2022  2:54:39.24 6097

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -933,7 +933,6 @@ class DashboardController extends Controller
 
     public function sendAgainNotification(Request $request)
     {
-        dd($request->all());
         $this->validate($request,array(
             'type'         => 'required',
             'headings'     => 'required',
