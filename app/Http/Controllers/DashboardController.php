2f291fcce873de04f1b9ca9de20f0a7824cf9319
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 02:24:49 +0600

    Commit Wed 09/28/2022  2:24:49.23 28161

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -902,7 +902,7 @@ class DashboardController extends Controller
             OneSignal::sendNotificationUsingTags(
                 "Some Message",$request->message,
                 $url = null,
-                $filters = array($tags),
+                $filters = array(array($tags)),
                 $data = null,
                 $buttons = null,
                 $schedule = null,
