0ddd0df7e0ad1a4da1302c401d1f7b68732b5059
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 01:29:24 +0600

    Commit Wed 09/28/2022  1:29:24.28 9348

--- resources/views/dashboard/notifications/index.blade.php
+++ resources/views/dashboard/notifications/index.blade.php
@@ -128,7 +128,8 @@
                       </div>
                       <div style="margin: 10px; padding: 10px; border: 1px solid #9C9FA0; border-radius: 10px;">
                         <small><span class="fas fa-bell"></span> BJS & Bar Exam</small><br/>
-                        asd
+                        <span id="headingstext"></span>
+                        <span id="headingstext"></span>
                       </div>
                 </div>
                 <div class="modal-footer">
