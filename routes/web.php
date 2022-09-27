2aca14f72d2dd438824ff5a6580246a2a6341b64
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 01:56:04 +0600

    Commit Wed 09/28/2022  1:56:04.26 16178

--- routes/web.php
+++ routes/web.php
@@ -86,7 +86,7 @@ Route::get('/dashboard/messages/delete/{id}', 'DashboardController@deleteMessage
 
 Route::get('/dashboard/notifications', 'DashboardController@getNotifications')->name('dashboard.notifications');
 Route::post('/dashboard/notifications/send', 'DashboardController@sendNotification')->name('dashboard.notifications.send');
-t('/dashboard/notifications/delete/{id}', 'DashboardController@deleteNotification')->name('dashboard.notifications.delete');
+Route::get('/dashboard/notifications/delete/{id}', 'DashboardController@deleteNotification')->name('dashboard.notifications.delete');
 
 
 
