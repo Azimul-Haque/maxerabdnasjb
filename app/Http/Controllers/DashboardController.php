08c094d15ef191932eea21ea8d3aa00fb3d6283c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 02:12:08 +0600

    Commit Wed 09/28/2022  2:12:08.26 26690

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -895,7 +895,7 @@ class DashboardController extends Controller
             OneSignal::sendNotificationUsingTags(
                 "Some Message",$request->message,
                 array(
-                    ["field" => "tag", "key" => "user_type", "relation" => "=", "value" => "email21@example.com"],
+                    ["field" => "tag", "key" => "user_type", "relation" => "=", "value" => "Premium"],
                     ...
                 ),
                 $url = null,
