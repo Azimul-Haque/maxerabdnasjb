2ae056fdc660590ec3fe01d7d9103de8dcadac38
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 02:03:44 +0600

    Commit Wed 09/28/2022  2:03:44.31 12000

--- resources/views/dashboard/notifications/index.blade.php
+++ resources/views/dashboard/notifications/index.blade.php
@@ -2,6 +2,7 @@
 @section('title') ড্যাশবোর্ড | নোটিফিকেশন @endsection
 
 @section('third_party_stylesheets')
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
 @endsection
 
