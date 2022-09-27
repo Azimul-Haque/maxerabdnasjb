a0a686aaaaeebdf55f734b66316d9b425884493f
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-28 01:30:37 +0600

    Commit Wed 09/28/2022  1:30:37.30 23578

--- resources/views/dashboard/notifications/index.blade.php
+++ resources/views/dashboard/notifications/index.blade.php
@@ -148,12 +148,8 @@
 @section('third_party_scripts')
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript">
-        $('#adduserrole').change(function () {
-            if($('#adduserrole').val() == 'accountant') {
-                $('#ifaccountant').hide();
-            } else {
-                $('#ifaccountant').show();
-            }
+        $('#headings').keyup(function () {
+          $('#headingstext').text(this.value);
         });
     </script>
 @endsection
