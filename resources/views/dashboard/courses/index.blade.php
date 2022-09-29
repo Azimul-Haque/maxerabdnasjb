0012b08c82fb6ecc76379e5f1d9280112d06d5e8
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-29 22:02:58 +0600

    Commit Thu 09/29/2022 22:02:58.22 3956

--- resources/views/dashboard/courses/index.blade.php
+++ resources/views/dashboard/courses/index.blade.php
@@ -189,7 +189,7 @@
                     <div class="input-group mb-3">
                         <select name="status" class="form-control" required>
                             <option selected="" disabled="" value="">স্ট্যাটাস</option>
-                            <option value="1">Active</option>
+                            <option value="1" selected>Active</option>
                             <option value="0">In-active</option>
                         </select>
                         <div class="input-group-append">
