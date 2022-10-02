d82aa68f66518caabcc024434ffee78cee7b3e7e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-02 16:37:41 +0600

    Commit Sun 10/02/2022 16:37:41.22 9454

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -175,6 +175,61 @@ class QuestionController extends Controller
         return redirect()->route('dashboard.questions');
     }
 
+    public function storeExcelQuestion(Request $request)
+    {
+        // dd($request->file('image'));
+        $this->validate($request,array(
+            'topic_id'    => 'required|string|max:191',
+            'question'    => 'required|string|max:191',
+            'option1'     => 'required|string|max:191',
+            'option2'     => 'required|string|max:191',
+            'option3'     => 'required|string|max:191',
+            'option4'     => 'required|string|max:191',
+            'answer'      => 'required',
+            'difficulty'  => 'required|string|max:191',
+            'image'       => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg,webp|max:3000',
+            'explanation' => 'sometimes|max:2048',
+        ));
+
+        $question             = new Question;
+        $question->topic_id   = $request->topic_id;
+        $question->question   = $request->question;
+        $question->option1    = $request->option1;
+        $question->option2    = $request->option2;
+        $question->option3    = $request->option3;
+        $question->option4    = $request->option4;
+        $question->answer     = $request->answer;
+        $question->difficulty = $request->difficulty;
+        $question->save();
+
+        
+        if(isset($request->tags_ids)){
+            $question->tags()->sync($request->tags_ids, false);
+        }
+
+        // image upload
+        if($request->hasFile('image')) {
+            $image    = $request->file('image');
+            $filename = random_string(5) . time() .'.' . "webp";
+            $location = public_path('images/questions/'. $filename);
+            Image::make($image)->resize(350, null, function ($constraint) { $constraint->aspectRatio(); })->save($location);
+            $questionimage              = new Questionimage;
+            $questionimage->question_id = $question->id;
+            $questionimage->image       = $filename;
+            $questionimage->save();
+        }
+
+        if($request->explanation) {
+            $questionexplanation              = new Questionexplanation;
+            $questionexplanation->question_id = $question->id;
+            $questionexplanation->explanation = $request->explanation;
+            $questionexplanation->save();
+        }
+
+        Session::flash('success', 'Question created successfully!');
+        return redirect()->route('dashboard.questions');
+    }
+
     public function updateQuestion(Request $request, $id)
     {
         // dd($request->file('image'));
