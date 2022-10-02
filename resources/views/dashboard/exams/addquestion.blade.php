e73840c84297cfcf3aecb66a361a02ea04c4133c
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-02 13:29:17 +0600

    Commit Sun 10/02/2022 13:29:17.28 16976

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -17,7 +17,7 @@
                       <h3 class="card-title">প্রশ্নসমূহ ({{ $examquestions->count() }} টি প্রশ্ন)</h3>
           
                       <div class="card-tools">
-                          <button type="button" class="btn btn-warning btn-sm"  data-toggle="modal" data-target="#addExamQuestionModal">
+                          <button type="button" class="btn btn-warning btn-sm"  data-toggle="modal" data-target="#addTAGQuestionModal">
                               <i class="fas fa-tasks"></i> ট্যাগ থেকে প্রশ্ন
                           </button>
                           <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addExamQuestionModal">
