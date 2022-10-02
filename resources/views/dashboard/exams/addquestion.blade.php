44dbcd2c50accc6c220a387cfdad6ce3052ec040
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-02 13:31:26 +0600

    Commit Sun 10/02/2022 13:31:26.30 20953

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -153,41 +153,6 @@
                     <div class="modal-body">
                         @csrf
                         <input type="hidden" name="exam_id" value="{{ $exam->id }}">
-                        <input type="hidden" id="hiddencheckarray" name="hiddencheckarray" value="{{ $questionchecktext }}">
-                        <table class="table table-condensed" id="datatablemodal">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>প্রশ্ন</th>
-                                    <th>টপিক</th>
-                                    <th>ট্যাগ</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                
-                                @foreach ($questions as $question)
-                                <tr>
-                                    <td>
-                                        <div class="icheck-primary icheck-inline" style="float: left;">
-                                            <input type="checkbox" onchange="checkboxquestion({{ $question->id }})" id="check{{ $question->id }}" name="questioncheck[]" value="{{ $question->id }}" 
-                                            @if(in_array($question->id, $examquestionidarray)) checked="" @endif
-                                            />
-                                            <label for="check{{ $question->id }}"> </label>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        {{ $question->question }}
-                                    </td>
-                                    <td><span class="badge bg-success">{{ $question->topic->name }}</span></td>
-                                    <td>
-                                        @foreach($question->tags as $tag)
-                                          <span class="badge bg-primary">{{ $tag->name }}</span>
-                                        @endforeach
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
