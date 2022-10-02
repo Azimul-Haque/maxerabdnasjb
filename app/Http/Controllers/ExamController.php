439c4bf3a3eb151d01e2d5cbabb27f332d45f700
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-02 14:18:09 +0600

    Commit Sun 10/02/2022 14:18:09.26 7606

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -261,7 +261,7 @@ class ExamController extends Controller
         $temptagquestions2 = array_unique($temptagquestions, SORT_REGULAR);
         array_diff_key($temptagquestions);
         // explode(",", implode(",", array_unique(explode(",", $temptagquestions))));
-        dd($temptagquestions);
+        dd($temptagquestions, $temptagquestions2);
         // foreach($tag->questions as $question) {
         //     $examquestion = new Examquestion;
         //     $examquestion->exam_id = $request->exam_id;
