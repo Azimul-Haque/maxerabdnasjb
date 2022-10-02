955dcb613e6b616d1666563bf2e7ae60bed2c7e7
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-02 17:34:08 +0600

    Commit Sun 10/02/2022 17:34:08.21 2910

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -188,43 +188,43 @@ class QuestionController extends Controller
 
         dd($collections);
         foreach ($collections as $collection) {
-            
-        }
+            $question             = new Question;
+            $question->topic_id   = $request->topic_id;
+            $question->question   = $request->question;
+            $question->option1    = $request->option1;
+            $question->option2    = $request->option2;
+            $question->option3    = $request->option3;
+            $question->option4    = $request->option4;
+            $question->answer     = $request->answer;
+            $question->difficulty = $request->difficulty;
+            $question->save();
 
-        $question             = new Question;
-        $question->topic_id   = $request->topic_id;
-        $question->question   = $request->question;
-        $question->option1    = $request->option1;
-        $question->option2    = $request->option2;
-        $question->option3    = $request->option3;
-        $question->option4    = $request->option4;
-        $question->answer     = $request->answer;
-        $question->difficulty = $request->difficulty;
-        $question->save();
+            
+            if(isset($request->tags_ids)){
+                $question->tags()->sync($request->tags_ids, false);
+            }
+
+            // image upload
+            if($request->hasFile('image')) {
+                $image    = $request->file('image');
+                $filename = random_string(5) . time() .'.' . "webp";
+                $location = public_path('images/questions/'. $filename);
+                Image::make($image)->resize(350, null, function ($constraint) { $constraint->aspectRatio(); })->save($location);
+                $questionimage              = new Questionimage;
+                $questionimage->question_id = $question->id;
+                $questionimage->image       = $filename;
+                $questionimage->save();
+            }
+
+            if($request->explanation) {
+                $questionexplanation              = new Questionexplanation;
+                $questionexplanation->question_id = $question->id;
+                $questionexplanation->explanation = $request->explanation;
+                $questionexplanation->save();
+            }
+        }
 
         
-        if(isset($request->tags_ids)){
-            $question->tags()->sync($request->tags_ids, false);
-        }
-
-        // image upload
-        if($request->hasFile('image')) {
-            $image    = $request->file('image');
-            $filename = random_string(5) . time() .'.' . "webp";
-            $location = public_path('images/questions/'. $filename);
-            Image::make($image)->resize(350, null, function ($constraint) { $constraint->aspectRatio(); })->save($location);
-            $questionimage              = new Questionimage;
-            $questionimage->question_id = $question->id;
-            $questionimage->image       = $filename;
-            $questionimage->save();
-        }
-
-        if($request->explanation) {
-            $questionexplanation              = new Questionexplanation;
-            $questionexplanation->question_id = $question->id;
-            $questionexplanation->explanation = $request->explanation;
-            $questionexplanation->save();
-        }
 
         Session::flash('success', 'Question created successfully!');
         return redirect()->route('dashboard.questions');
