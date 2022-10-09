b181d54a5cdbd1d07ff040c20b9694ebac772a0b
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-10 05:18:35 +0600

    Commit Mon 10/10/2022  5:18:35.23 10334

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -132,7 +132,7 @@ class APIController extends Controller
             $courses = Cache::remember('courses', 300, function () {
                  $courses = Course::select('id', 'name')
                              ->where('status', 1) // take only active courses
-                             ->where('type', $coursetype) // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT, 5 = QB
+                             ->where('type', 1) // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT, 5 = QB
                              ->orderBy('priority', 'asc')
                              ->get();
                  foreach($courses as $course) {
