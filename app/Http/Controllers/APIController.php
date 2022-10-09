ff28e05003eeba9ce90fbd6fb63e1d17814645b4
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-10 04:51:46 +0600

    Commit Mon 10/10/2022  4:51:46.32 25979

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -132,7 +132,7 @@ class APIController extends Controller
                              ->where('status', 1) // take only active courses
                              ->where('type', $coursetype) // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT, 5 = QB
                              ->orderBy('priority', 'asc')
-                             ->get();ss
+                             ->get();
             foreach($courses as $course) {
                 $course->examcount = $course->courseexams->count();
                 $course->makeHidden('courseexams');
