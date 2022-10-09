4921e38b18f18008d57c7b4de6f2c5a3e3f6f665
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-10 05:28:44 +0600

    Commit Mon 10/10/2022  5:28:44.33 22446

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -129,7 +129,7 @@ class APIController extends Controller
     {
         if($softtoken == 'Rifat.Admin.2022')
         {
-            $courses = Cache::remember('courses$coursetype', 300, function () use ($coursetype) {
+            $courses = Cache::remember('courses'+$coursetype, 300, function () use ($coursetype) {
                  $courses = Course::select('id', 'name')
                              ->where('status', 1) // take only active courses
                              ->where('type', $coursetype) // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT, 5 = QB
