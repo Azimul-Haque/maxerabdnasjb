2a8bb4940eb2324bfc540f514020d6f8b07113dc
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-10 05:21:57 +0600

    Commit Mon 10/10/2022  5:21:57.29 2276

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -141,7 +141,17 @@ class APIController extends Controller
                  }
                  return $courses;
             });
-                            ->get();
+
+            $courses = Course::select('id', 'name')
+                        ->where('status', 1) // take only active courses
+                        ->where('type', 1) // 1 = Course, 2 = BJS MT, 3 = Bar MT, 4 = Free MT, 5 = QB
+                        ->orderBy('priority', 'asc')
+                        ->get();
+            foreach($courses as $course) {
+                $course->examcount = $course->courseexams->count();
+                $course->makeHidden('courseexams');
+            }
+            
             
             // dd($courses->all());
             echo response()->json([
