b3ecac884c7077a4644600450e1da3df4625af55
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-31 21:25:15 +0600

    Commit Mon 10/31/2022 21:25:15.27 26751

--- resources/views/bkash/bkash-script.blade.php
+++ resources/views/bkash/bkash-script.blade.php
@@ -98,6 +98,7 @@
         $.post('{{ route('bkash-success') }}', {
             payment_info: data
         }, function (res) {
+            alert('পেমেন্ট সাকসেসফুল!')
             location.reload();
         });
     }
