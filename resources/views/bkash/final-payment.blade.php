c4b0bd0838389002ab787f5f7e74184994ddd7c1
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-01 01:25:43 +0600

    Commit Tue 11/01/2022  1:25:43.31 6953

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -61,10 +61,17 @@
           
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="text" placeholder="Search">
-        <button class="btn btn-primary" type="button">Search</button>
-      </form>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Link</a>
+        </li>
+      </ul>
     </div>
   </div>
 </nav>
