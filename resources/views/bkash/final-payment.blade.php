dddc334706ae5291946131e650107efc275a5293
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-31 20:42:51 +0600

    Commit Mon 10/31/2022 20:42:51.34 22381

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -45,7 +45,7 @@
     
     <div class="card p-3">
         <div class="d-flex justify-content-between align-items-center mt-2 mb-2">
-            <big>পেমেন্ট ৳ ২.০০</big>
+            <big>পেমেন্ট ৳ {{ bangla($amount) }}</big>
         </div>
         
         <button class="btn btn-danger" id="bKash_button" onclick="BkashPayment()" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
