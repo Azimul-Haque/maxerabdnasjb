b573dc2dc6daa3a1ea8b9d13545298cd747dcc73
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-01 01:27:33 +0600

    Commit Tue 11/01/2022  1:27:33.31 1659

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -43,33 +43,41 @@
     }
 </style>
 
-<nav class="navbar navbar-expand-sm navbar-light bg-light">
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="javascript:void(0)">bKash Production Test</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="mynavbar">
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          
-        </li>
-        <li class="nav-item">
-          
-        </li>
-        <li class="nav-item">
-          
-        </li>
-      </ul>
+          <li class="nav-item">
+
+          </li>
+          <li class="nav-item">
+
+          </li>
+          <li class="nav-item">
+
+          </li>
+        </ul>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
+          <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
+          <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
+          <a class="nav-link" href="#">Link</a>
+        </li>  
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Dropdown</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Link</a></li>
+            <li><a class="dropdown-item" href="#">Another link</a></li>
+            <li><a class="dropdown-item" href="#">A third link</a></li>
+          </ul>
         </li>
       </ul>
     </div>
