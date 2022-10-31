124395d3b10748aa459ea9784ac50c48bd6904c6
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-31 20:36:24 +0600

    Commit Mon 10/31/2022 20:36:24.23 24712

--- resources/views/bkash/bkash-payment.blade.php
+++ resources/views/bkash/bkash-payment.blade.php
@@ -1,45 +1,9 @@
 <title>bKash Production Test</title>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-
-{{-- <script id="myScript"
-        src="https://scripts.sandbox.bka.sh/versions/1.2.0-beta/checkout/bKash-checkout-sandbox.js"></script> --}}
-
-{{--    This Commented Script for Live Production --}}
-<script id="myScript" src="https://scripts.pay.bka.sh/versions/1.2.0-beta/checkout/bKash-checkout.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-
-
-{{-- <button class="btn btn-success" id="bKash_button" onclick="BkashPayment()">
-    Pay with bKash
-</button> --}}
-
-<style>
-    .overlay{
-        display: none;
-        position: fixed;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        z-index: 999;
-        background: rgba(224,34,95,0.3) url("/images/Spinner-1s-200px.gif") center no-repeat;
-    }
-    body{
-        text-align: center;
-    }
-    /* Turn off scrollbar when body element has the loading class */
-    body.loading{
-        overflow: hidden;   
-    }
-    /* Make spinner image visible when body element has the loading class */
-    body.loading .overlay{
-        display: block;
-    }
-</style>
-<div class="overlay"></div>
 
 <div class="height d-flex justify-content-center align-items-center">
     
@@ -75,7 +39,7 @@
             
         </div>
         
-        <button class="btn btn-danger" id="bKash_button" onclick="BkashPayment()" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
+        <button class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
         </button>
     </div>
 
@@ -113,13 +77,10 @@
     
         </div>
     
-        <button class="btn btn-danger" id="bKash_button" onclick="BkashPayment()" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
+        <button class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
         </button>
     </div>
 </div>
-
-
-@include('bkash.bkash-script')
 
 <style type="text/css">
         body{
