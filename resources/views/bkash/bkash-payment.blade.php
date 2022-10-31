1f2ed0c00e71dff06f14676528a3458b2dfcaf25
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-31 20:37:01 +0600

    Commit Mon 10/31/2022 20:37:01.29 13963

--- resources/views/bkash/bkash-payment.blade.php
+++ resources/views/bkash/bkash-payment.blade.php
@@ -39,8 +39,8 @@
             
         </div>
         
-        <button href="{{ route('bkash-prod-test-payment') }}" class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
-        </button>
+        <a href="{{ route('bkash-prod-test-payment') }}" class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
+        </a>
     </div>
 
     <div style="width:20px"></div>
@@ -77,8 +77,8 @@
     
         </div>
     
-        <button href="{{ route('bkash-prod-test-payment') }}" class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
-        </button>
+        <a href="{{ route('bkash-prod-test-payment') }}" class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
+        </a>
     </div>
 </div>
 
