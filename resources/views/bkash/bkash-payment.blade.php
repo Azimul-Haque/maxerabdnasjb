c7acc495595aeccf5819419539c06447ec7cc491
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-31 19:43:08 +0600

    Commit Mon 10/31/2022 19:43:08.24 9223

--- resources/views/bkash/bkash-payment.blade.php
+++ resources/views/bkash/bkash-payment.blade.php
@@ -1,5 +1,4 @@
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 {{-- <script id="myScript"
         src="https://scripts.sandbox.bka.sh/versions/1.2.0-beta/checkout/bKash-checkout-sandbox.js"></script> --}}
