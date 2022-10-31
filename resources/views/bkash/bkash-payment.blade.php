09cfc9c4358aa8ec216af8601cd0e5a48ac387e0
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-01 01:22:08 +0600

    Commit Tue 11/01/2022  1:22:08.24 31839

--- resources/views/bkash/bkash-payment.blade.php
+++ resources/views/bkash/bkash-payment.blade.php
@@ -1,9 +1,9 @@
 <title>bKash Production Test</title>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 <div class="height d-flex justify-content-center align-items-center">
     
