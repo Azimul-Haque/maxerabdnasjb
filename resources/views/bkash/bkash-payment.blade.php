b57b67bdcaa9b0cab096b8965f522e342b40481a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-10-31 20:14:21 +0600

    Commit Mon 10/31/2022 20:14:21.25 1061

--- resources/views/bkash/bkash-payment.blade.php
+++ resources/views/bkash/bkash-payment.blade.php
@@ -111,7 +111,9 @@
     
         </div>
     
-        <button class="btn btn-danger" id="bKash_button" onclick="BkashPayment()">বিকাশ করুন</button>
+        <button class="btn btn-danger" id="bKash_button" onclick="BkashPayment()">
+                <img src="{{ asset('images/bkash.gif') }}">
+        </button>
     </div>
 </div>
 
