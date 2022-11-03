8a495dab0f4473aa64e1a4609f1a38ef531a42a5
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-03 14:08:56 +0600

    Commit Thu 11/03/2022 14:08:56.24 24742

--- resources/views/bkash/bkash-payment.blade.php
+++ resources/views/bkash/bkash-payment.blade.php
@@ -159,7 +159,7 @@
         
         <form>
             @csrf
-            <button href="{{ route('bkash-prod-test-payment', '200.00') }}" class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
+            <button href="{{ route('bkash-prod-test-payment') }}" class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
             </button>
         </form>
     </div>
