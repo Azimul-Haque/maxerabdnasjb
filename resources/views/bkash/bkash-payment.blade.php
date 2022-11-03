8ad6c18b811b792a3362a1eaeb2192e70f592f2a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-03 14:07:38 +0600

    Commit Thu 11/03/2022 14:07:38.63 7161

--- resources/views/bkash/bkash-payment.blade.php
+++ resources/views/bkash/bkash-payment.blade.php
@@ -157,7 +157,7 @@
     
         </div>
     
-        <a href="{{ route('bkash-prod-test-payment', '5.00') }}" class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
+        <a href="{{ route('bkash-prod-test-payment', '200.00') }}" class="btn btn-danger" id="bKash_button" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
         </a>
     </div>
 </div>
