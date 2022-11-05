bb5733783fd719fb5291b2540e0b6d828a46811d
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-05 16:29:10 +0600

    Commit Sat 11/05/2022 16:29:10.34 11236

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -52,7 +52,7 @@
 
 <div class="overlay"></div>
 <br/><br/>
-<div class="d-flex justify-content-center align-items-center">
+<div class="height d-flex justify-content-center align-items-center">
     
     <div class="card p-3">
         <div class="d-flex justify-content-between align-items-center mt-2 mb-2">
