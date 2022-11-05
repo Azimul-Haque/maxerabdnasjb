fa395da68cc986eac01a5aa8996d39a27618f0ae
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-05 16:16:53 +0600

    Commit Sat 11/05/2022 16:16:53.26 11015

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -10,6 +10,9 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<script type="text/javascript">
+    
+</script>
 
 
 {{-- <button class="btn btn-success" id="bKash_button" onclick="BkashPayment()">
