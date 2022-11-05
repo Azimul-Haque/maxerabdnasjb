5fa060b7d36d6cc21f361333755e6a7116a38fd9
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-05 16:23:34 +0600

    Commit Sat 11/05/2022 16:23:34.36 24927

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -12,7 +12,7 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <script type="text/javascript">
     setTimeout(
-        BkashPayment,
+        console.log('ttt'),
     1000);
 </script>
 
