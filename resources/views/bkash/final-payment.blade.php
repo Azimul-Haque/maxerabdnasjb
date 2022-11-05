091e6e7a141ea68cb3a7dc580b074e7f90f11666
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-05 16:17:25 +0600

    Commit Sat 11/05/2022 16:17:25.26 15028

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -12,7 +12,7 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <script type="text/javascript">
     $(function(){
-       yourfunction();
+       BkashPayment();
     });
 </script>
 
