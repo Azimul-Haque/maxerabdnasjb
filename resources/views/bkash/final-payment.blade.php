677d5ca4bd579ce7c61f08d035d5cfd576a3f7ae
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-05 16:41:42 +0600

    Commit Sat 11/05/2022 16:41:42.54 29615

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -1,6 +1,6 @@
 <title>bKash Production Test</title>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
