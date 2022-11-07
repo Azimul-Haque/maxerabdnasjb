32eee45198768dd60e3d9d92e8d54ed09cfbbed5
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-07 14:42:17 +0600

    Commit Mon 11/07/2022 14:42:17.28 10334

--- resources/views/bkash/bkash-script.blade.php
+++ resources/views/bkash/bkash-script.blade.php
@@ -104,8 +104,8 @@
                 mobile: {{ $mobile }},
                 package_id: {{ $packageid }}
             }, function (res) {
-                alert('Payment is successful' + res);
                 console.log(res.);
+                alert('Payment is successful' + res);
                 // location.reload();
                 // window.location.replace('{{ route('bkash-prod-test') }}');
             });
