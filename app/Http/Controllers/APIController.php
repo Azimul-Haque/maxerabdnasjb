ccd54594ac9e21fd0964dea1c4bfe005a0cffc52
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-15 02:11:54 +0600

    Commit Tue 11/15/2022  2:11:54.26 12872

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -346,7 +346,7 @@ class APIController extends Controller
         if($user && $softtoken == 'Rifat.Admin.2022')
         {
             foreach($user->payments as $payment) {
-                $payment->makeHidden(['id', 'user_id', 'package_id', 'uid', '']);
+                $payment->makeHidden(['id', 'user_id', 'package_id', 'uid', 'payment_status', '']);
             }
             dd($user->payments);
             return response()->json([
