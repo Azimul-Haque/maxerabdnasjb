beaed4de3728e4b93779dc4f44682ab10c1c029a
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-15 13:07:26 +0600

    Commit Tue 11/15/2022 13:07:26.28 4208

--- resources/views/dashboard/messages/index.blade.php
+++ resources/views/dashboard/messages/index.blade.php
@@ -36,7 +36,7 @@
                 	<tr>
                     <td>
                       @if($message->status == 1)
-                        <i class="far fa-check-square"></i>
+                        <i class="fas fa-calendar-check"></i>
                       @endif
                       {{ $message->user->name }}
                     </td>
