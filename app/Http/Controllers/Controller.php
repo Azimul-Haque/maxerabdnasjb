5123b7e68d15a432f896eca446947e534c5f9c66
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-15 13:17:30 +0600

    Commit Tue 11/15/2022 13:17:30.29 1708

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct() 
+    {
+      $sharedbasicinfo = Basicinfo::find(1);
+
+        
+
+      View::share('sharedbasicinfo', $sharedbasicinfo);
+      View::share('notifpendingapplications', $notifpendingapplications);
+    }
 }
