af80f62f66f78392951d382566aea44c3dad20ba
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-15 13:08:49 +0600

    Commit Tue 11/15/2022 13:08:49.26 27704

--- resources/views/dashboard/messages/index.blade.php
+++ resources/views/dashboard/messages/index.blade.php
@@ -63,7 +63,7 @@
                           <div class="modal-dialog" role="document">
                             <div class="modal-content">
                               <div class="modal-header bg-danger">
-                                <h5 class="modal-title" id="updateModalLabel">মেসেজ ডিলেট</h5>
+                                <h5 class="modal-title" id="updateModalLabel">সমাধান করা হয়েছে?</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                   <span aria-hidden="true">&times;</span>
                                 </button>
