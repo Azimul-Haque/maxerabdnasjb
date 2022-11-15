413426567200739994ed78fba3443161af58d603
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-15 13:08:34 +0600

    Commit Tue 11/15/2022 13:08:34.25 23544

--- resources/views/dashboard/messages/index.blade.php
+++ resources/views/dashboard/messages/index.blade.php
@@ -59,11 +59,11 @@
                         {{-- Delete Modal Code --}}
                         {{-- Delete Modal Code --}}
                         <!-- Modal -->
-                        <div class="modal fade" id="updateModal{{ $message->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true" data-backdrop="static">
+                        <div class="modal fade" id="updateModal{{ $message->id }}" tabindex="-1" role="dialog" aria-labelledby="updateModalLabel" aria-hidden="true" data-backdrop="static">
                           <div class="modal-dialog" role="document">
                             <div class="modal-content">
                               <div class="modal-header bg-danger">
-                                <h5 class="modal-title" id="deleteModalLabel">মেসেজ ডিলেট</h5>
+                                <h5 class="modal-title" id="updateModalLabel">মেসেজ ডিলেট</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                   <span aria-hidden="true">&times;</span>
                                 </button>
