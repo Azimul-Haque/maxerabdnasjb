202ddff6a310c97216c8c9a67ae0b6f7ff01f051
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-15 13:14:05 +0600

    Commit Tue 11/15/2022 13:14:05.31 15458

--- resources/views/dashboard/messages/index.blade.php
+++ resources/views/dashboard/messages/index.blade.php
@@ -76,7 +76,7 @@
                               </div>
                               <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">ফিরে যান</button>
-                                <form method="PUT" action="{{ route('dashboard.messages.update', $message->id) }}">
+                                <form method="POST" action="{{ route('dashboard.messages.update', $message->id) }}">
                                   @csrf
                                   <button type="submit" class="btn btn-warning">দাখিল করুন</button>
                                 </form>
