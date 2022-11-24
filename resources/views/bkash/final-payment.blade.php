fe84f0df39eae822e08535998321acd1f6c5d59e
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-25 00:04:51 +0600

    Commit Fri 11/25/2022  0:04:51.31 27945

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -64,9 +64,10 @@
         
         <button class="btn btn-danger" id="bKash_button" onclick="BkashPayment()" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
         </button>
-    </div><br/>
-    <p>DO NOT CLOSE THE PAGE</p>
-</div>
+    </div>
+
+</div><br/>
+<p>DO NOT CLOSE THE PAGE</p>
 
 
 @include('bkash.bkash-script')
