34882c11a5b0c85d5fb93288335b3b0e2b4969d6
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-25 00:18:24 +0600

    Commit Fri 11/25/2022  0:18:24.28 7157

--- resources/views/bkash/final-payment.blade.php
+++ resources/views/bkash/final-payment.blade.php
@@ -64,6 +64,7 @@
         
         <button class="btn btn-danger" id="bKash_button" onclick="BkashPayment()" style="background: url({{ asset('images/bkash_payment_logo.png') }}); background-size: 100%; background-size: 250px auto; background-repeat: no-repeat;">
         </button>
+        <p>উপরের বাটনে ক্লিক করুন</p>
     </div>
 </div>
 
