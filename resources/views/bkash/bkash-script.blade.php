d88ce6fdd72850e4cf0256e24244998f3ffad116
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-25 00:22:38 +0600

    Commit Fri 11/25/2022  0:22:38.24 11605

--- resources/views/bkash/bkash-script.blade.php
+++ resources/views/bkash/bkash-script.blade.php
@@ -139,6 +139,6 @@
         }
         function hideLoading() {
             $("body").removeClass("loading"); 
-            $("#changeTextonLoad").text("loading"); 
+            $("#changeTextonLoad").text("Do NOT close the page!"); 
         }
 </script>
