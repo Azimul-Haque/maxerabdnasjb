87c4311e9adfe171b751bebf76030fefcfab2382
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-26 01:57:58 +0600

    Commit Sat 11/26/2022  1:57:58.31 2792

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -86,6 +86,7 @@ class DashboardController extends Controller
     public function clearQueryCache()
     {
         Cache::flush();
+        Session::flash('success', 'সকল কোয়েরি ক্যাশ মুছে দেওয়া হয়েছে!');
         return redirect()->route('dashboard.index');
     }
 
