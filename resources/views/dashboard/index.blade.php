987e3aada2e97560e7263630e0ca290fadd9d4ed
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-26 00:41:53 +0600

    Commit Sat 11/26/2022  0:41:53.27 21541

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -28,7 +28,7 @@
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                <h4>{{ $totalmonthlypayment->totalamount }}<sup style="font-size: 20px">৳</sup></h4>
+                <h4>{{ $totalmonthlypayment->totalamount ? $totalmonthlypayment->totalamount : 0 }}<sup style="font-size: 20px">৳</sup></h4>
 
                 <p>মাসিক আয় ({{ date('F Y') }})</p>
               </div>
