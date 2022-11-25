938738567878b4645ed1ae7afb0fab42964d4d34
Azimul-Haque <orbachinujbuk@gmail.com>
2022-11-26 01:53:06 +0600

    Commit Sat 11/26/2022  1:53:06.23 28869

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -99,11 +99,11 @@
             {{-- Modal Code --}}
             {{-- Modal Code --}}
             <!-- Modal -->
-            <div class="modal fade" id="deleteQuestionModal{{ $question->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteQuestionModalLabel" aria-hidden="true" data-backdrop="static">
+            <div class="modal fade" id="clearQueryCacheModal" tabindex="-1" role="dialog" aria-labelledby="clearQueryCacheModalLabel" aria-hidden="true" data-backdrop="static">
                 <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header bg-danger">
-                    <h5 class="modal-title" id="deleteQuestionModalLabel">প্রশ্ন ডিলেট</h5>
+                    <h5 class="modal-title" id="clearQueryCacheModalLabel">প্রশ্ন ডিলেট</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
