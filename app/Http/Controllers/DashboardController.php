502ac444761c789b2e8ca3e4f1d174a578295a33
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-04 23:12:52 +0600

    Commit Sat 03/04/2023 23:12:52.26 11694

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -101,49 +101,11 @@ class DashboardController extends Controller
     public function getUser($id)
     {
         $user = User::find($id);
-        $balances = Balance::where('receiver_id', $id)
-                           ->orderBy('id', 'desc')
-                           ->paginate(10);
-
-        $todaystotalexpense = DB::table('expenses')
-                                ->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') as created_at"), DB::raw('SUM(amount) as totalamount'))
-                                ->where('user_id', $id)
-                                ->where(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d')"), "=", Carbon::now()->format('Y-m-d'))
-                                ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d')"))
-                                ->first();
-        $monthlytotalexpense = DB::table('expenses')
-                                        ->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m') as created_at"), DB::raw('SUM(amount) as totalamount'))
-                                        ->where('user_id', $id)
-                                        ->where(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"), "=", Carbon::now()->format('Y-m'))
-                                        ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"))
-                                        ->first();
-        $totalexpense = Expense::where('user_id', $id)->sum('amount');
-
-        $todaystotaldeposit = DB::table('balances')
-                                ->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d') as created_at"), DB::raw('SUM(amount) as totalamount'))
-                                ->where('receiver_id', $id)
-                                ->where(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d')"), "=", Carbon::now()->format('Y-m-d'))
-                                ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d')"))
-                                ->first();
-        $monthlytotaldeposit = DB::table('balances')
-                                        ->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m') as created_at"), DB::raw('SUM(amount) as totalamount'))
-                                        ->where('receiver_id', $id)
-                                        ->where(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"), "=", Carbon::now()->format('Y-m'))
-                                        ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"))
-                                        ->first();
-        $totaldeposit = Balance::where('receiver_id', $id)->sum('amount');
         
         // dd($totaldeposit);
 
         return view('dashboard.dashboard.users.single')
-                    ->withUser($user)
-                    ->withBalances($balances)
-                    ->withTodaystotalexpense($todaystotalexpense)
-                    ->withMonthlytotalexpense($monthlytotalexpense)
-                    ->withTotalexpense($totalexpense)
-                    ->withTodaystotaldeposit($todaystotaldeposit)
-                    ->withMonthlytotaldeposit($monthlytotaldeposit)
-                    ->withTotaldeposit($totaldeposit);
+                    ->withUser($user);
     }
 
     public function getUserWithOtherPage($id)
