f90452521b83d49ba28a3a7334448dec35b8a461
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-04 23:14:14 +0600

    Commit Sat 03/04/2023 23:14:14.32 24043

--- resources/views/dashboard/users/single.blade.php
+++ resources/views/dashboard/users/single.blade.php
@@ -70,16 +70,7 @@
         <div class="card-body p-0">
           <table class="table">
             <tbody>
-              {{-- <tr>
-                <td>1.</td>
-                <td>Update software</td>
-                <td>
-                  <div class="progress progress-xs">
-                    <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                  </div>
-                </td>
-                <td><span class="badge bg-danger">55%</span></td>
-              </tr> --}}
+              
               @foreach($balances as $balance)
                 <tr>
                   <td style="line-height: 1;">
