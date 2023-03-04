39a479e1bcd92e24833915179889a14c49b9fc40
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-04 23:16:16 +0600

    Commit Sat 03/04/2023 23:16:16.29 7175

--- resources/views/dashboard/users/single.blade.php
+++ resources/views/dashboard/users/single.blade.php
@@ -40,18 +40,6 @@
           </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-6">
-          <div class="info-box mb-3">
-            <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-balance-scale-right"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">সর্বমোট ব্যালেন্স</span>
-              <span class="info-box-number">৳ </span>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
 @endsection
 
