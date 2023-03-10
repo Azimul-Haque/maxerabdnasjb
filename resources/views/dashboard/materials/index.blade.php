2ec9604e47e67f1550f8c6fefe27dd091de37962
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-11 05:34:05 +0600

    Commit Sat 03/11/2023  5:34:05.25 22765

--- resources/views/dashboard/materials/index.blade.php
+++ resources/views/dashboard/materials/index.blade.php
@@ -359,7 +359,7 @@
 
 {{-- <script type="text/javascript" src="//code.jquery.com/jquery-3.6.0.min.js"></script> --}}
 <script src="https://cdn.ckeditor.com/ckeditor5/12.3.1/classic/ckeditor.js"></script>
-<script type="text/javascript" src="cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+{{-- <script type="text/javascript" src="cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
     // $('.multiple-select').select2({
