b31f0d9b9648a6ca9941f448f544a93aa8c112c4
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-11 03:39:40 +0600

    Commit Sat 03/11/2023  3:39:40.28 16057

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -168,7 +168,6 @@ class APIController extends Controller
             $courseexams = Cache::remember('courseexams'.$id, 7 * 24 * 60 * 60, function () use ($id) {
                 $courseexams = Courseexam::select('course_id', 'exam_id')
                                      ->where('course_id', $id)
-                                     ->orderBy('created_at', 'desc')
                                      ->get();
 
                 foreach($courseexams as $courseexam) {
