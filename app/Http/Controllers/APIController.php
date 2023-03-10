18683177d4d93c73652d5eab629109077dbaf647
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-10 22:38:25 +0600

    Commit Fri 03/10/2023 22:38:25.29 20612

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -267,7 +267,7 @@ class APIController extends Controller
     {
         if($softtoken == 'Rifat.Admin.2022')
         {
-            $topicquestions = Question::select('')
+            $topicquestions = Question::select('question', 'option1', 'option2', 'option2')
                                       ->where('topic_id', $id)->orderBy(DB::raw('RAND()'))->take(20)->get();
 
             dd($topicquestions);
