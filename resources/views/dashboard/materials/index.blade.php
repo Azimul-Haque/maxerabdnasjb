8c9f106df076650a192c86008af5fc63f803688f
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-11 05:18:22 +0600

    Commit Sat 03/11/2023  5:18:22.25 18744

--- resources/views/dashboard/materials/index.blade.php
+++ resources/views/dashboard/materials/index.blade.php
@@ -299,6 +299,14 @@
                             </div>
                         </div>    
                       </div>
+                      <div class="col-md-6">
+                        <div class="input-group mb-3">
+                            <input type="text" name="author_desc" class="form-control" value="{{ old('author_desc') }}" placeholder="লেখকের ডেসিগনেশন" required>
+                            <div class="input-group-append">
+                                <div class="input-group-text"><span class="far fa-user"></span></div>
+                            </div>
+                        </div>    
+                      </div>
                       
                     </div>
                     <div class="row">
