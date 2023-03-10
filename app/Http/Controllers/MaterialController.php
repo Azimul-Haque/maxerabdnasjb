7a2ff4f9f7eb28b19cc381ac31e1ad91eaa6424e
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-11 05:57:58 +0600

    Commit Sat 03/11/2023  5:57:58.27 29615

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -79,75 +79,29 @@ class MaterialController extends Controller
     {
         // dd($request->file('image'));
         $this->validate($request,array(
-            'topic_id'    => 'required|string|max:191',
-            'question'    => 'required|string|max:255',
-            'option1'     => 'required|string|max:191',
-            'option2'     => 'required|string|max:191',
-            'option3'     => 'required|string|max:191',
-            'option4'     => 'required|string|max:191',
-            'answer'      => 'required',
-            'difficulty'  => 'required|string|max:191',
-            'image'       => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg,webp|max:3000',
-            'explanation' => 'sometimes|max:2048',
+            'type'    => 'required|integer',
+            'title'    => 'required|string|max:255',
+            'author'    => 'required|string|max:255',
+            'author_desc'    => 'required|string|max:255',
+            'content'    => 'required',
+            'url'     => 'required|string|max:255',
+            'status'     => 'required|integer',
         ));
 
         // dd($request->tags_ids);
 
-        $question             = Question::findOrFail($id);
-        $question->topic_id   = $request->topic_id;
-        $question->question   = $request->question;
-        $question->option1    = $request->option1;
-        $question->option2    = $request->option2;
-        $question->option3    = $request->option3;
-        $question->option4    = $request->option4;
-        $question->answer     = $request->answer;
-        $question->difficulty = $request->difficulty;
-        $question->save();
+        $material             = new Material;
+        $material->type   = $request->type;
+        $material->title   = $request->title;
+        $material->author    = $request->author;
+        $material->author_desc    = $request->author_desc;
+        $material->content    = $request->content;
+        $material->url    = $request->url;
+        $material->status     = $request->status;
+        $material->save();
 
-        if(isset($request->tags_ids)){
-            $question->tags()->sync($request->tags_ids, true);
-        }
-
-        // image upload
-        if($request->hasFile('image')) {
-            if($question->questionimage) {
-                $image_path = public_path('images/questions/'. $question->questionimage->image);
-                if(File::exists($image_path)) {
-                    File::delete($image_path);
-                }
-                $question->questionimage->delete();
-            }
-            $image      = $request->file('image');
-            $filename   = random_string(5) . time() .'.' . "webp";
-            $location   = public_path('images/questions/'. $filename);
-            Image::make($image)->resize(350, null, function ($constraint) { $constraint->aspectRatio(); })->save($location);
-            $questionimage = new Questionimage;
-            $questionimage->question_id = $question->id;
-            $questionimage->image = $filename;
-            $questionimage->save();
-        }
-
-        if($request->explanation) {
-            if($question->questionexplanation) {
-                $question->questionexplanation->explanation = $request->explanation;
-                $question->questionexplanation->save();
-            } else {
-                $questionexplanation = new Questionexplanation;
-                $questionexplanation->question_id = $question->id;
-                $questionexplanation->explanation = $request->explanation;
-                $questionexplanation->save();
-            }
-        }
-
-        Session::flash('success', 'Question updated successfully!');
-        // return redirect()->route('dashboard.questions');
+        Session::flash('success', 'Material updated successfully!');
         return redirect()->back();
-        // dd(url()->previous());
-        // if(request()->routeIs('dashboard.questionstopicbased')) {
-        //     return redirect()->route('dashboard.questionstopicbased', $request->topic_id);
-        // } else {
-        //     return redirect()->route('dashboard.questions');
-        // }
     }
 
     public function deleteMaterial($id)
