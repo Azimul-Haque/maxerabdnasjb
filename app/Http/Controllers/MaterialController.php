5ceca88183d7b433683e674a3b3c5d57d4e44941
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-11 04:59:46 +0600

    Commit Sat 03/11/2023  4:59:46.38 19312

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -19,5 +19,29 @@ use Redirect;
 
 class MaterialController extends Controller
 {
-    //
+    public function __construct()
+    {
+        parent::__construct();
+        $this->middleware('auth');
+        $this->middleware(['admin'])->only('storeQuestionsTopic', 'storeQuestionsTag', 'deleteQuestion');
+        // $this->middleware(['manager'])->only();
+    }
+
+    public function getQuestions()
+    {
+        if(!(Auth::user()->role == 'admin' || Auth::user()->role == 'manager')) {
+            abort(403, 'Access Denied');
+        }
+        
+        $totalquestions = Question::count();
+        $questions = Question::orderBy('id', 'desc')->paginate(10);
+        $topics = Topic::orderBy('id', 'asc')->get();
+        $tags = Tag::orderBy('id', 'asc')->get();
+
+        return view('dashboard.questions.index')
+                    ->withQuestions($questions)
+                    ->withTopics($topics)
+                    ->withTags($tags)
+                    ->withTotalquestions($totalquestions);
+    }
 }
