b1fe3b9905ad584aebe305c83775bb757a7180a8
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-10 23:30:56 +0600

    Commit Fri 03/10/2023 23:30:56.29 55

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -269,7 +269,7 @@ class APIController extends Controller
         {
             $topicquestions = Question::select('question', 'option1', 'option2', 'option2', 'option3', 'option4', 'answer')
                                       ->where('topic_id', $id)->orderBy(DB::raw('RAND()'))
-                                      ->take(20)
+                                      // ->take(20)
                                       ->get();
                                       
             foreach($topicquestions as $topicquestion) {
