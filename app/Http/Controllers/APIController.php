a9a20993e1f5d1e7d4d7ed511b137d8731161bc7
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-10 22:36:23 +0600

    Commit Fri 03/10/2023 22:36:23.23 17617

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -272,7 +272,7 @@ class APIController extends Controller
             // dd($topicquestions);
             foreach($topicquestions as $topicquestion) {
                 dd($topicquestion);
-                $topicquestion->question = $topicquestion->question->makeHidden(['topic_id', 'difficulty', 'created_at', 'updated_at', 'questionexplanation', 'questionimage']);
+                $topicquestion->makeHidden(['topic_id', 'difficulty', 'created_at', 'updated_at', 'questionexplanation', 'questionimage']);
             }
             $exam = Exam::findOrFail($id);
             $exam->participation++;
