d2c4588595779afd40a97120ff3be3b3f45f6cd3
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-11 06:09:04 +0600

    Commit Sat 03/11/2023  6:09:04.28 24079

--- resources/views/dashboard/materials/index.blade.php
+++ resources/views/dashboard/materials/index.blade.php
@@ -65,12 +65,7 @@
                                     {{ $material->author_desc }}
                                   </td>
                                   <td>{{ $material->type }}</td>
-                                  <td>{{ $material->option1 }}, {{ $material->option2 }}, {{ $material->option3 }}, {{ $material->option4 }}</td>
-                                  {{-- <td>
-                                      <div class="progress progress-xs">
-                                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                      </div>
-                                  </td> --}}
+                                  
                               
                                   <td>
                                       <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editMaterialModal{{ $material->id }}">
