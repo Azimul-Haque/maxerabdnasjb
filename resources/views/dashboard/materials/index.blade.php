1d0aadad95e7cf354fe5a19d59b7d19d0c9e26f2
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-11 06:14:57 +0600

    Commit Sat 03/11/2023  6:14:57.23 25031

--- resources/views/dashboard/materials/index.blade.php
+++ resources/views/dashboard/materials/index.blade.php
@@ -325,21 +325,7 @@
 
 {{-- <script type="text/javascript" src="cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<script>
-    // $('.multiple-select').select2({
-    //   // theme: 'bootstrap4',
-    // });
-    ClassicEditor
-        .create( document.querySelector( '.ckeditor' ), {
-          placeholder: 'কন্টেন্ট লিখুন...',
-        } )
-        .then( editor => {
-                console.log( editor );
-        } )
-        .catch( error => {
-                console.error( error );
-        } );
-</script>
+
 <script type="text/javascript">
   // $(document).ready(function() {
   //   $('#summernote').summernote();
