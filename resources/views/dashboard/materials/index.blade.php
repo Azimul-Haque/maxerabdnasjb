a0e3adf4f324db7401941ce454e70614f3b72fca
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-11 06:15:05 +0600

    Commit Sat 03/11/2023  6:15:05.31 17156

--- resources/views/dashboard/materials/index.blade.php
+++ resources/views/dashboard/materials/index.blade.php
@@ -7,6 +7,21 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="{{ asset('js/select2.full.min.js') }}"></script>
 <script src="https://cdn.ckeditor.com/ckeditor5/12.3.1/classic/ckeditor.js"></script>
+<script>
+    // $('.multiple-select').select2({
+    //   // theme: 'bootstrap4',
+    // });
+    ClassicEditor
+        .create( document.querySelector( '.ckeditor' ), {
+          placeholder: 'কন্টেন্ট লিখুন...',
+        } )
+        .then( editor => {
+                console.log( editor );
+        } )
+        .catch( error => {
+                console.error( error );
+        } );
+</script>
 <style type="text/css">
   .select2-selection__choice{
       background-color: rgba(0, 123, 255) !important;
