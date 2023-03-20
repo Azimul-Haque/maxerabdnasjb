b1383bd4f6c290423f7fba4eeade5b96dafd663a
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-21 02:08:39 +0600

    Commit Tue 03/21/2023  2:08:39.28 7068

--- resources/views/dashboard/exams/addquestionbankall.blade.php
+++ resources/views/dashboard/exams/addquestionbankall.blade.php
@@ -145,7 +145,7 @@
     $(document).ready( function() {
       $(document).on('click', '#search-button', function() {
         if($('#search-param').val() != '') {
-          var urltocall = '{{ route('dashboard.index') }}' +  '/dashboard/exams/add/question/bank/search/{{ $exam->id }}/' + $('#search-param').val();
+          var urltocall = '{{ route('dashboard.index') }}' +  '/exams/add/question/bank/search/{{ $exam->id }}/' + $('#search-param').val();
           location.href= urltocall;
         } else {
           $('#search-param').css({ "border": '#FF0000 2px solid'});
@@ -158,7 +158,7 @@
       $("#search-param").keyup(function(e) {
         if(e.which == 13) {
           if($('#search-param').val() != '') {
-            var urltocall = '{{ route('dashboard.index') }}' +  '/dashboard/exams/add/question/bank/search/{{ $exam->id }}/' + $('#search-param').val();
+            var urltocall = '{{ route('dashboard.index') }}' +  '/exams/add/question/bank/search/{{ $exam->id }}/' + $('#search-param').val();
             location.href= urltocall;
           } else {
             $('#search-param').css({ "border": '#FF0000 2px solid'});
