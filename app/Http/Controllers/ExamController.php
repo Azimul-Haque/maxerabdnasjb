adadfebc4c5e23ff3652e3d8897c2f36d87b649b
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-20 23:54:48 +0600

    Commit Mon 03/20/2023 23:54:48.22 7216

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -218,21 +218,11 @@ class ExamController extends Controller
             'questioncheck'    => 'required',
         ));
 
-        
         $oldexamquestions = Examquestion::where('exam_id', $request->exam_id)->get();
         if(count($oldexamquestions) > 0) {
             foreach($oldexamquestions as $oldexamquestion) {
                 $oldexamquestion->delete();
             }
-        }
-        $hiddencheckarray = explode(',', $request->hiddencheckarray);
-        // sort($hiddencheckarray);
-        // dd($hiddencheckarray);
-        foreach($hiddencheckarray as $question_id) {
-            $examquestion = new Examquestion;
-            $examquestion->exam_id = $request->exam_id;
-            $examquestion->question_id = $question_id;
-            $examquestion->save();
         }
 
         Session::flash('success', 'Question updated successfully!');
