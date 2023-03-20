4d947a55abe3d4318ec4efde66df16470971189b
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-21 01:42:19 +0600

    Commit Tue 03/21/2023  1:42:19.35 4286

--- resources/views/dashboard/exams/addquestionbankall.blade.php
+++ resources/views/dashboard/exams/addquestionbankall.blade.php
@@ -4,6 +4,7 @@
 @section('third_party_stylesheets')
 <link href="{{ asset('css/select2.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('css/select2-bootstrap4.min.css') }}" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css">
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="{{ asset('js/select2.full.min.js') }}"></script>
 <style type="text/css">
