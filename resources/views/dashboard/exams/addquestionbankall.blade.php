92e47bac985a9429b99e81d52bac17f48370758b
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-21 02:08:09 +0600

    Commit Tue 03/21/2023  2:08:09.28 18177

--- resources/views/dashboard/exams/addquestionbankall.blade.php
+++ resources/views/dashboard/exams/addquestionbankall.blade.php
@@ -158,7 +158,7 @@
       $("#search-param").keyup(function(e) {
         if(e.which == 13) {
           if($('#search-param').val() != '') {
-            var urltocall = '{{ route('dashboard.questions') }}' +  '/' + $('#search-param').val();
+            var urltocall = '{{ route('dashboard.index') }}' +  '/dashboard/exams/add/question/bank/search/{{ $exam->id }}/' + $('#search-param').val();
             location.href= urltocall;
           } else {
             $('#search-param').css({ "border": '#FF0000 2px solid'});
