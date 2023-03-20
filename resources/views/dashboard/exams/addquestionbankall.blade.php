f43486fdd708a94cac9ab3fdc03bf5fc41f7b000
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-21 01:38:53 +0600

    Commit Tue 03/21/2023  1:38:53.25 10052

--- resources/views/dashboard/exams/addquestionbankall.blade.php
+++ resources/views/dashboard/exams/addquestionbankall.blade.php
@@ -38,6 +38,7 @@
                       <table class="table">
                           <thead>
                               <tr>
+                                  <th>#</th>
                                   <th>Question</th>
                                   <th>Answer</th>
                                   <th width="25%">Options</th>
@@ -55,6 +56,14 @@
                               @endphp
                               @foreach($questions as $question)
                                 <tr>
+                                    <td>
+                                        <div class="icheck-primary icheck-inline" style="float: left;">
+                                            <input type="checkbox" onchange="checkboxquestion({{ $question->id }})" id="check{{ $question->id }}" name="questioncheck[]" value="{{ $question->id }}" 
+                                            @if(in_array($question->id, $examquestionidarray)) checked="" @endif
+                                            />
+                                            <label for="check{{ $question->id }}"> </label>
+                                        </div>
+                                    </td>
                                     <td>
                                         {{ $question->question }}<br/>
                                         <span class="badge bg-success">{{ $question->topic->name }}</span>
