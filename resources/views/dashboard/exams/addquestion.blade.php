d9115e7dbb48cf8658993d2f7ba4e921dcdfc570
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-20 23:04:35 +0600

    Commit Mon 03/20/2023 23:04:35.32 4687

--- resources/views/dashboard/exams/addquestion.blade.php
+++ resources/views/dashboard/exams/addquestion.blade.php
@@ -29,10 +29,10 @@
                         <button type="submit" class="btn btn-danger btn-sm"  data-toggle="modal" data-target="#addTAGQuestionModal">
                             <i class="fas fa-tags"></i> ক্লিয়ার করুন
                         </button>
-                        <button type="button" class="btn btn-warning btn-sm"  data-toggle="modal" data-target="#addTAGQuestionModal" style="margin-left: 5px;">
+                        <button type="button" class="btn btn-warning btn-sm"  data-toggle="modal" data-target="#addTAGQuestionModal">
                             <i class="fas fa-tags"></i> ট্যাগ থেকে প্রশ্ন
                         </button>
-                        <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addExamQuestionModal" style="margin-left: 5px;">
+                        <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addExamQuestionModal">
                             <i class="fas fa-tasks"></i> প্রশ্ন হালনাগাদ করুন
                         </button>
                           <form method="post" action="{{ route('dashboard.exams.question.clear') }}" enctype='multipart/form-data' class="form-inline">
