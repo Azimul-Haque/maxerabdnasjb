905e9513bbec03dda1a5deefe5eb75a9743632a6
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-29 01:47:16 +0600

    Commit Wed 03/29/2023  1:47:16.28 1409

--- resources/views/dashboard/users/single.blade.php
+++ resources/views/dashboard/users/single.blade.php
@@ -31,7 +31,7 @@
 
             <div class="info-box-content">
                 <big><b>{{ $user->name }}</b></big>
-                <span class="info-box-text">Firebase UID: <u>{{ $user->uid }}</u></span>
+                <span class="info-box-text">Firebase UID: <u><i>{{ $user->uid }}</i></u></span>
                 <span class="info-box-text">Onesignal Player ID: <u>{{ $user->onesignal_id }}</u></span>
             </div>
           </div>
