d56cfab38096ba7cd8920237b8a9b4bd9b3545b0
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-29 01:49:40 +0600

    Commit Wed 03/29/2023  1:49:40.29 3102

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -142,11 +142,11 @@
             				                        <div class="input-group-text"><span class="fas fa-phone"></span></div>
             				                    </div>
             				                </div>
-                                    
+
                                     <div class="input-group mb-3">
                                         <input type="text"
-                                               name="mobile"
-                                               value="{{ $user->mobile }}"
+                                               name="uid"
+                                               value="{{ $user->uid }}"
                                                autocomplete="off"
                                                class="form-control"
                                                placeholder="মোবাইল নম্বর (১১ ডিজিট)" required>
