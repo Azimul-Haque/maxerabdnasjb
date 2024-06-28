e05482a1840dc217c4b3a891dc8a0a93c4a11788
Azimul-Haque <orbachinujbuk@gmail.com>
2024-06-29 02:09:26 +0600

    Commit Sat 06/29/2024  2:09:26.33 14461

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -243,57 +243,6 @@ class IndexController extends Controller
 
     public function requestACDelete(Request $request)
     {
-        if($request->get('pay_status') == 'Failed') {
-            Session::flash('info', 'পেমেন্ট সম্পন্ন হয়নি, আবার চেষ্টা করুন!');
-            return redirect()->route('index.index');
-        }
-        
-        $amount_request = $request->get('opt_b');
-        $amount_paid = $request->get('amount');
-
-        if($request->pay_status == "Successful" && $amount_paid == $amount_request) {
-            // OLD VERIFICATION METHOD
-            
-            $temppayment = Temppayment::where('trx_id', $request->mer_txnid)->first();
-            // dd($request->all());
-            $payment = new Payment;
-            $payment->user_id = $temppayment->user_id;
-            $payment->package_id = $temppayment->package_id;
-            $payment->uid = $temppayment->uid;
-            $payment->payment_status = 1;
-            $payment->card_type = $request->card_type;
-            $payment->trx_id = $request->mer_txnid;
-            $payment->amount = $request->amount;
-            $payment->store_amount = $request->store_amount;
-            $payment->save();
-
-            $user = User::findOrFail($temppayment->user_id);
-            $current_package_date = Carbon::parse($user->package_expiry_date);
-            $package = Package::findOrFail($temppayment->package_id);
-            if($current_package_date->greaterThanOrEqualTo(Carbon::now())) {
-                $package_expiry_date = $current_package_date->addDays($package->numeric_duration)->format('Y-m-d') . ' 23:59:59';
-            } else {
-                $package_expiry_date = Carbon::now()->addDays($package->numeric_duration)->format('Y-m-d') . ' 23:59:59';
-            }
-            // dd($package_expiry_date);
-            $user->package_expiry_date = $package_expiry_date;
-            $user->save();
-            // ARO KAAJ THAKTE PARE, JODI FIREBASE EO UPDATE KORA LAAGE
-            // ARO KAAJ THAKTE PARE, JODI FIREBASE EO UPDATE KORA LAAGE
-            // dd($payment);
-
-            $temppayment->delete();
-
-            Session::flash('swalsuccess', 'পেমেন্ট সফল হয়েছে। অ্যাপটি ব্যবহার করুন। ধন্যবাদ!');
-            // return redirect()->route('index.index');
-        } else {
-            // dd($request->all());
-            // $paymentdata = json_encode($request->all());
-            // Session::flash('swalsuccess', $paymentdata);
-            Session::flash('info', 'পেমেন্ট সম্পন্ন হয়নি, অনুগ্রহ করে Contact ফর্ম এর মাধ্যমে আমাদের জানান।');
-            // return redirect()->route('index.index');
-        }
-
         Session::flash('swalsuccess', 'Thank you for the deletion request. We will update you soon!');
         return redirect()->route('index.index');
     }
